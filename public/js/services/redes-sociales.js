e87d054bf87c367f4c706ea98acda34a1f7282e6
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2015-04-09 09:46:10 -0500

    redes sociales sin integración con usuario

---
+++ public/js/services/redes-sociales.js
@@ -0,0 +1,6 @@
+(function(){
+	angular.module('gemStore')
+	.factory('RedFactory', ['$resource', 'Constantes',function($resource,Constantes) {
+		return $resource(Constantes.url+'/redes/:id');
+	}]);
+})();
