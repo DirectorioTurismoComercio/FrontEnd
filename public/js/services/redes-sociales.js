d49c026954a932a468960053bf02d97b2abfd596
thecap <xthecapx@gmail.com>
2015-04-09 17:36:03 -0500

    Overlay Funcional y arregladas las imagenes

--- public/js/services/redes-sociales.js
+++
@@ -1,6 +0,0 @@
-(function(){
-	angular.module('gemStore')
-	.factory('RedFactory', ['$resource', 'Constantes',function($resource,Constantes) {
-		return $resource(Constantes.url+'/redes/:id');
-	}]);
-})();
--- public/js/services/usuario-redes.js
+++
@@ -1,6 +0,0 @@
-(function(){
-	angular.module('gemStore')
-	factory('UsuarioRedesFactory', ['$resource', 'configuracion',function($resource,configuracion) {
-	return $resource(configuracion.url+'/usuarios/:idusuario/redes');
-	}]);
-})();
