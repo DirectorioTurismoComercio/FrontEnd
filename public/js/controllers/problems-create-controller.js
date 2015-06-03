fef4722f0fbc51108899a994c6a69104d39959ab
ANDRES KWAN <andreskwan@gmail.com>
2015-06-02 20:06:53 -0500

    CRUD - complete

--- public/js/controllers/problems-create-controller.js
+++ public/js/controllers/problems-create-controller.js
@@ -7,13 +7,11 @@ angular.module('gemStore')
 .controller('ProblemsCreateController', function(ProblemFactory, $scope, $routeParams, $location){
 	$scope.problem = new ProblemFactory();
 	//TODO: create category model
-	$scope.problem.category =[];
-	$scope.problem.usuario =Number($routeParams.idUser);
-	debugger;
-	$scope.isSubmitting = false;
-
-	$scope.saveProblem = function(problem){
-		$scope.isSubmitting = true;
+	$scope.problem.categoria=[];
+	$scope.problem.usuario  =Number($routeParams.idUser);
+	$scope.isSubmitting     =false;
+	$scope.saveProblem      =function(problem){
+		$scope.isSubmitting =true;
 		problem.$save({id:$routeParams.idUser})
 		.then(function(problem){
 			// console.log("------this is the problem returned from server: ",problem);
