5f1da75941b274d7abb0bc26697b2ec7bea95d63
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2015-06-06 12:28:51 -0500

    se eliminó Constantes y resources de SuggestionsFactory. Se resolvió el query con promesas. Se cambió la variable i por token

--- public/js/services/result-retriever.js
+++ public/js/services/result-retriever.js
@@ -1,37 +1,34 @@
 (function(){
 	angular.module('gemStore')
-	.factory('ResultRetriever', ['$resource', 'Constantes','$q','$timeout', 'SuggestionsFactory',
-		function($resource,Constantes,$q,$timeout,SuggestionsFactory) {
+	.factory('ResultRetriever', ['$q','$timeout', 'SuggestionsFactory',
+		function($q,$timeout,SuggestionsFactory) {
 		  var ResultRetriever = new Object();
 
-		  ResultRetriever.getresults = function(i) {
+		  ResultRetriever.getresults = function(token) {
 		    var resultdata = $q.defer();
 		    var results;
 
-		    var someResults = ["1 The Wolverine", "The Smurfs 2", "The Mortal Instruments: City of Bones", "Drinking Buddies", "All the Boys Love Mandy Lane", "The Act Of Killing", "Red 2", "Jobs", "Getaway", "Red Obsession", "2 Guns", "The World's End", "Planes", "Paranoia", "The To Do List", "Man of Steel"];
-
-		    var moreResults; // = ["crear página web","crear aplicación web", "crear aplicación aplicación móvil", "necesito desarrollador","crear sitio en internet","paginas internet"];
 		      
-		      SuggestionsFactory.query({token: i},
-		      function(data)
+		      SuggestionsFactory.query({token: token}).$promise.
+		      then(
+		      function(results)
 		      {
-		      	console.log(data);
-		        moreResults=data;
-		            if(i && i.indexOf('T')!=-1)
-		      results=someResults;
-		    else
-		      results=moreResults;
-
-		  console.log("more results"+moreResults);
-
-		    $timeout(function(){
-		      resultdata.resolve(results);
-		    },1000);
+		      		  $timeout(function(){
+				      resultdata.resolve(results);
+				    },1000);
 		      }
+		      )
+		      .catch(function(errors){
+                console.log("Error al recuperar los roles desde el servidor: ",errors);
+	            })
+		      .finally(function(){
+	               
+	            });
 
 
 
-        );
+
+        
 
 
 		    return resultdata.promise
