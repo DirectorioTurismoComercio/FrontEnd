a8b203e80618d3d6d009f6af32690f2538598e47
ANDRES KWAN <andreskwan@gmail.com>
2015-07-09 08:57:59 -0500

    Cascading select/dropdowns - working but need some refactoring

--- public/js/controllers/problems-create-controller.js
+++ public/js/controllers/problems-create-controller.js
@@ -6,30 +6,48 @@
 */
 angular.module('gemStore')
 .controller('ProblemsCreateController', 
-	['ProblemFactory', '$scope', '$routeParams', '$location','ResultRetriever',
-	function(ProblemFactory, $scope, $routeParams, $location,ResultRetriever){
+	['CategoryFactory','ProblemFactory', '$scope', '$routeParams', '$location','ResultRetriever',
+	function(CategoryFactory, ProblemFactory, $scope, $routeParams, $location,ResultRetriever){
+
 	$scope.problem = new ProblemFactory();
-	//TODO: create category model
+	
+
 	$scope.problem.categoria=[];
 	$scope.problem.tags=[];
 	$scope.problem.usuario  =Number($routeParams.idUser);
-	$scope.isSubmitting     =false;
+	$scope.isSubmitting     =false;	
+
+	//categoreis related code
+	// $scope.categories = CategoryFactory.query({nivel:"2",categoria_padre:"1"});
+	$scope.rootCategories = CategoryFactory.query({nivel:"1"});
+	$scope.getCategoriesLevel2 = function(idParent,level){
+		console.log("Level2 idParent",idParent);
+		$scope.subcategoryTwo = CategoryFactory.query({categoria_padre:idParent,nivel:level});
+		console.log($scope.subcategoryTwo);
+	};
+
+	$scope.getCategoriesLevel3 = function(idParent,level){
+		console.log("Level3 idParent",idParent);
+		$scope.subcategoryThree = CategoryFactory.query({categoria_padre:idParent,nivel:level});
+		console.log($scope.subcategoryThree);
+	};
+	//tags related code
 	$scope.tag = { result:  ""};
+
 	$scope.doSomething = function(typedthings){
       $scope.results = ResultRetriever.getresults(typedthings, 'SuggestedTagsFactory');
 
       $scope.results.then(function(data){
         $scope.results = data;
       });
-    }
+    };
 	$scope.remove_tag = function(index){	
        $scope.problem.tags.splice(index,1);    
-	}
+	};
 	$scope.add_tag = function(new_tag){
 		if($scope.problem.tags.indexOf(new_tag)==-1){
 			$scope.problem.tags.push(new_tag);	
 		}
-		
 		$scope.tag.result = "";
 	}
 
@@ -37,8 +55,11 @@ angular.module('gemStore')
       $scope.add_tag(suggestion);
       	$scope.tag.result = "";
     }
+
+    //
 	$scope.saveProblem      =function(problem){
 		$scope.isSubmitting =true;
+		console.log("problem.rootCategory: ",problem.rootCategory);
 		problem.$save({id:$routeParams.idUser})
 		.then(function(problem){
 			// console.log("------this is the problem returned from server: ",problem);
