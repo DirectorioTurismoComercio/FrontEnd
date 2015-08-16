5e736a23f2a4c88494d3900041e252e11cf2e8e0
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2015-08-16 10:48:39 -0500

    gitignore modfied

--- public/js/services/suggestions-factory.js
+++
@@ -1,6 +0,0 @@
-(function(){
-	angular.module('gemStore')
-	.factory('SuggestionsFactory', ['$resource', 'Constantes',function($resource,Constantes) {
-		return $resource(Constantes.url+'/sugerencias',{token: '@token'});
-	}]);
-})();
--- test/pruebas_aceptacion/tests/acceptance/registro_usuarioCept.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('quiero registrarme');
-$I->amOnPage('/#/user');
-$I->see('Registro de Usuario');
-?>
