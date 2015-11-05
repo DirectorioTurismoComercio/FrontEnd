7f4513c5554b16639a9f33f565399ea8c9eea9c6
luispacholopez <luispacholopez@gmail.com>
2015-11-05 09:32:09 -0500

    Commit Profile
    Falta apariencia de búsquedas ya estoy elaborandola para que la pueda
    subir

--- public/js/controllers/profile/profile-search-detail-controller.js
+++ public/js/controllers/profile/profile-search-detail-controller.js
@@ -8,7 +8,32 @@
           questionnaireService.setQuestionnaires(datos.cuestionario);
           $scope.questionnaires = questionnaireService.getQuestionnaires();                  
           console.log($scope.questionnaires);
-          // $scope.questionnaires[0].enable = true;
+          // for (var i = 0; i <= $scope.questionnaires.length-1; i++) {
+          //   $scope.questionnaires[i].enable = true;  
+          // }; 
+          var cont = 0;               
+          for(var k=0;k<$scope.questionnaires.length;k++)
+          {  
+            $scope.questionnaires[k].enable=true;
+            for(var i=0; i<$scope.questionnaires[k].preguntas.length;i++)
+            { 
+              cont = 0; 
+              for(var j=0;j<$scope.questionnaires[k].preguntas[i].pregunta.opciones.length;j++)
+              {                
+                if ($scope.questionnaires[k].preguntas[i].pregunta.opciones[j].dato === 1) {
+                  cont++;
+                  $scope.questionnaires[k].preguntas[i].dato = $scope.questionnaires[k].preguntas[i].pregunta.opciones[j].dato;
+                } else{
+                  $scope.questionnaires[k].preguntas[i].pregunta.opciones[j].dato === 0;                  
+                };                
+              }            
+              if (cont > 0) {     
+                $scope.questionnaires[k].preguntas[i].enable=true;             
+              } else{
+                $scope.questionnaires[k].preguntas[i].enable=false;             
+              };
+            } 
+          }    
         console.log($scope.questionnaires);
       }).catch(function(error){
         console.log(error);
