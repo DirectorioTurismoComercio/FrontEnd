e239f52576f3c011b9ddaabeea45b547de8df030
Victor Rodriguez <vrodmz@gmail.com>
2016-02-22 12:14:09 -0500

    Proteccion de rutas consumiendo el metodo correcto para verificar si el usuario esta logeado

--- public/js/routes/router.decorator.js
+++ public/js/routes/router.decorator.js
@@ -2,7 +2,7 @@ angular.module('gemStore')
     .run(function ($rootScope, $location, autenticacionService) {
         $rootScope.$on('$routeChangeStart', function (event, next) {
             if (next.authenticate) {
-                if (!autenticacionService.isUserAuthenticated()) {
+                if (!autenticacionService.getInfo()) {
                     event.preventDefault();
                     $location.path('/signin');
                 }
