411f1c41f0c20a5b667d3c47f2493fc8991b9fc3
blackmantram <misantropee@gmail.com>
2016-02-22 14:13:08 -0500

    actualización del sample de las constantes

--- public/js/constants.sample.js
+++ public/js/constants.sample.js
@@ -4,3 +4,9 @@ angular.module('gemStore')
         "ruta_imagenes": "./images/",
         "app": "C"
     });
+
+angular.module('constants', [])
+    .constant("API_CONFIG", {
+    	"url": "http://127.0.0.1:8000",
+    	"login": "/rest-auth/login"
+    });
