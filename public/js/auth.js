df22eb06c76c2e8f83dcd7f5bbcf2c6986dba6c4
blackmantram <misantropee@gmail.com>
2016-02-22 10:01:15 -0500

    modulo de autenticacion ahora usa el api remoto

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,17 +1,26 @@
 (function () {
 	var auth = angular.module('auth',[]);
-	auth.factory('authenticationService', function(){
+	auth.factory('authenticationService', ['$http', 'API_CONFIG', function($http, API_CONFIG){
 		var user = null;
+
+		function checkForValidCredentials(c) {
+			if(c === undefined || c.password === undefined) 
+				throw Error("crendentials should contain username and password");
+		}
 			
 		return {
-			login: function(){
-				user = {};
+			login: function(credentials){
+				checkForValidCredentials(credentials);
+				$http.post(API_CONFIG.url + API_CONFIG.login);
+				user = {
+					name:credentials.username
+				};
 			},
 			getUser: function(){
 				return user;
 			}
 		}
-	});
+	}]);
 	auth.factory('isUserLoggedIn', ['authenticationService', function(authenticationService) {
 	    return function(){
 	        return !(authenticationService.getUser() === null);
