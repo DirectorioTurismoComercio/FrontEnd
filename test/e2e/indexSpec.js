ecb630ada21acb3a92a56ea10b5e6dc6624e9144
blackmantram <misantropee@gmail.com>
2016-02-23 13:14:45 -0500

    primera prueba e2e con protractor

---
+++ test/e2e/indexSpec.js
@@ -0,0 +1,7 @@
+describe('Index', function() {
+  it('should have a title', function() {
+    browser.get('http://localhost/ecosistema/FrontEnd/public/#/signin');
+
+    expect(browser.getTitle()).toEqual('mercatic');
+  });
+});
