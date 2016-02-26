399b7e570deb9fa5d657d103c68d1b06b2a9b6e3
AndresDiaz1 <desarrollo2@iMac-de-Vivelab.local>
2016-02-26 16:57:03 -0500

    Se ignora archivo conf.js de las pruebas e2e para poder cambiar la url del browser

--- test/e2e/authSpec.js
+++ test/e2e/authSpec.js
@@ -1,44 +1,44 @@
-describe('auth', function() {
+describe('auth', function () {
 
-	beforeAll(function(){
-		browser.get('http://localhost/ecosistema/FrontEnd/public/#/auth');
-		element(by.model('login.usuario')).sendKeys('misantropee@gmail.com');
-		element(by.model('login.contrasena')).sendKeys('1234');
-		element(by.id('login')).click();
-	});
+    beforeAll(function () {
+        browser.get(browser.baseUrl + '/FrontEnd/public/#/auth');
+        element(by.model('login.usuario')).sendKeys('misantropee@gmail.com');
+        element(by.model('login.contrasena')).sendKeys('1234');
+        element(by.id('login')).click();
+    });
 
-	it('redirects to profile after login', function() {
-		expect(browser.getCurrentUrl()).toMatch(/\/#\/profileMain/);	
- 	});
+    it('redirects to profile after login', function () {
+        expect(browser.getCurrentUrl()).toMatch(/\/#\/profileMain/);
+    });
 
-	it('should redirect to profile main if logged user forces main page URL', function(){
-		browser.navigateTo='http://localhost/ecosistema/FrontEnd/public/#/signin';
-		expect(browser.getCurrentUrl()).toMatch(/\/#\/profileMain/);
-	});
+    it('should redirect to profile main if logged user forces main page URL', function () {
+        browser.navigateTo = browser.baseUrl + '/FrontEnd/public/#/signin';
+        expect(browser.getCurrentUrl()).toMatch(/\/#\/profileMain/);
+    });
 
- 	it('redirects to profile after refresh', function() {
- 		browser.refresh();
-		expect(browser.getCurrentUrl()).toMatch(/\/#\/profileMain/);	
- 	});
+    it('redirects to profile after refresh', function () {
+        browser.refresh();
+        expect(browser.getCurrentUrl()).toMatch(/\/#\/profileMain/);
+    });
 
- 	it('keeps user info after refresh', function() {
- 		browser.refresh();
-		element.all(by.css('.datos_perfil')).evaluate('usuario')
-			.then(function(value){
-				var user = value[0];
-				expect(user.correo).toBe('misantropee@gmail.com');
-			});
- 	});
+    it('keeps user info after refresh', function () {
+        browser.refresh();
+        element.all(by.css('.datos_perfil')).evaluate('usuario')
+            .then(function (value) {
+                var user = value[0];
+                expect(user.correo).toBe('misantropee@gmail.com');
+            });
+    });
 
- 	it('redirects to main after logout', function() {
- 		element(by.id('menu')).click();
- 		element(by.id('logout')).click().then(function(){
-	 		browser.wait(function(){
-	 			return browser.getCurrentUrl().then(function(url) {
-	 				console.log(url, url.match(/\/#\/signin/));
-	                return url.match(/\/#\/signin/) != null;
-	            });
-	 		}, 5000);
- 		});
- 	});
+    it('redirects to main after logout', function () {
+        element(by.id('menu')).click();
+        element(by.id('logout')).click().then(function () {
+            browser.wait(function () {
+                return browser.getCurrentUrl().then(function (url) {
+                    console.log(url, url.match(/\/#\/signin/));
+                    return url.match(/\/#\/signin/) != null;
+                });
+            }, 5000);
+        });
+    });
 });
--- test/e2e/indexSpec.js
+++ test/e2e/indexSpec.js
@@ -1,6 +1,6 @@
-describe('Index', function() {
-	it('should have a title', function() {
-		browser.get('http://localhost/ecosistema/FrontEnd/public/#/signin');
-		expect(browser.getTitle()).toEqual('mercatic');
-	});
+describe('Index', function () {
+    it('should have a title', function () {
+        browser.get(browser.baseUrl + '/FrontEnd/public/#/signin');
+        expect(browser.getTitle()).toEqual('mercatic');
+    });
 });
