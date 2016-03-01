0e341584bbf5477c5af6164c81568b474eae834f
AndresDiaz1 <desarrollo2@iMac-de-Vivelab.local>
2016-03-01 14:46:07 -0500

    Se coloca un dropdown menu para elegir municipio en editar perfil

--- public/js/controllers/profile/profile-update-controller.js
+++ public/js/controllers/profile/profile-update-controller.js
@@ -1,40 +1,53 @@
-(function(){
-	angular.module('gemStore')
-	.controller('ProfileUpdateController', ['$scope','Constantes','autenticacionService','$location','UserByToken','navBar','LogoutFactory','questionnaireService',
-		function($scope,Constantes,autenticacionService,$location,UserByToken,navBar,LogoutFactory,questionnaireService){                        
-        $scope.ruta = Constantes.ruta_imagenes + "botones/";
-        $scope.anterior = $scope.ruta+'boton-regresar.png';
-        $scope.usuario = 	autenticacionService.getUser();
-        $scope.token = autenticacionService.getInfo();        
-        console.log($scope.usuario);
-        console.log($scope.token);
+(function () {
+    angular.module('gemStore')
+        .controller('ProfileUpdateController', ['$scope', 'Constantes', 'autenticacionService', '$location', 'UserByToken', 'navBar', 'LogoutFactory', 'questionnaireService', 'MunicipiosFactory',
+            function ($scope, Constantes, autenticacionService, $location, UserByToken, navBar, LogoutFactory, questionnaireService, MunicipiosFactory) {
+                $scope.ruta = Constantes.ruta_imagenes + "botones/";
+                $scope.anterior = $scope.ruta + 'boton-regresar.png';
+                $scope.usuario = autenticacionService.getUser();
+                $scope.token = autenticacionService.getInfo();
+                console.log($scope.usuario);
+                console.log($scope.token);
 
-        $scope.menu_bar = function (view){
-            questionnaireService.changeView(view);                      
-        }
+                MunicipiosFactory.query().$promise.then(function (an) {
+                    $scope.municipios = an;
+                }).catch(function (error) {
+                    console.log(error);
+                });
 
-        $scope.changepass = function(){
-          $location.path('/auth/changepass');
-        }
+                $scope.muni = function (index) {
+                    $scope.usuario.ubicacion_institucion = $scope.municipios[index].nombre;
+                    $scope.usuario.municipio_id = $scope.municipios[index];
+                }
 
-        $scope.update = function(){        	        	        	
-        	UserByToken.up(autenticacionService.getInfo()).update(
-            {"nombres": $scope.usuario.nombres,
-             "apellido1": $scope.usuario.apellido1,
-             "apellido2": $scope.usuario.apellido2,
-             "rol": $scope.usuario.rol,
-             "correo": $scope.usuario.correo,
-             "telefono_institucion": $scope.usuario.telefono_institucion,
-             "ubicacion_institucion": $scope.usuario.ubicacion_institucion
-            }).$promise.then(function(response){                                                                         
-              autenticacionService.setUser(response);                        
-              console.log(response);
-            }).catch(function(error){
-              console.log(error);            
-            });      
-          
-        }
-		}
-	]);
+                $scope.menu_bar = function (view) {
+                    questionnaireService.changeView(view);
+                }
+
+                $scope.changepass = function () {
+                    $location.path('/auth/changepass');
+                }
+
+                $scope.update = function () {
+                    UserByToken.up(autenticacionService.getInfo()).update(
+                        {
+                            "nombres": $scope.usuario.nombres,
+                            "apellido1": $scope.usuario.apellido1,
+                            "apellido2": $scope.usuario.apellido2,
+                            "rol": $scope.usuario.rol,
+                            "correo": $scope.usuario.correo,
+                            "telefono_institucion": $scope.usuario.telefono_institucion,
+                            "ubicacion_institucion": $scope.usuario.ubicacion_institucion,
+                            "municipio_id": $scope.usuario.municipio_id
+                        }).$promise.then(function (response) {
+                        autenticacionService.setUser(response);
+                        console.log(response);
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+
+                }
+            }
+        ]);
 
 })();
