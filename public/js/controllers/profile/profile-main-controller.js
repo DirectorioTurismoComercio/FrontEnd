8ed6ad598469c6e5d6f3e6796734579afe5d1a11
Victor Rodriguez <vrodmz@gmail.com>
2016-03-10 15:58:14 -0500

    Agregados los botones ver mapa y eliminados logs y variables innecesarias

--- public/js/controllers/profile/profile-main-controller.js
+++ public/js/controllers/profile/profile-main-controller.js
@@ -1,58 +1,33 @@
-(function(){
-	angular.module('gemStore')
-	.controller('ProfileMainController', ['$scope','Constantes','$location','questionnaireService','navBar','$mdToast','LogoutFactory','authenticationService','solutionService','RoleFactory',
-		function($scope,Constantes,$location,questionnaireService,navBar,$mdToast,LogoutFactory,authenticationService,solutionService,RoleFactory){                              	
-      $scope.usuario = authenticationService.getUser();
-      $scope.rol = $scope.usuario.rol;
-      questionnaireService.setRol($scope.rol);
+(function () {
+    angular.module('gemStore')
+        .controller('ProfileMainController', ['$scope', 'Constantes', '$location', 'questionnaireService', 'navBar', '$mdToast', 'LogoutFactory', 'authenticationService', 'solutionService', 'RoleFactory',
+            function ($scope, Constantes, $location, questionnaireService, navBar, $mdToast, LogoutFactory, authenticationService, solutionService, RoleFactory) {
+                $scope.usuario = authenticationService.getUser();
+                $scope.rol = $scope.usuario.rol;
+                questionnaireService.setRol($scope.rol);
 
-      var roleFactory     = new RoleFactory();
-      console.log("rol factory"+roleFactory);
-      RoleFactory.get({'id': $scope.rol}).$promise 
-      .then(function(inforol){
-        console.log(inforol);
-        questionnaireService.setImgRol(inforol.imagen);
-        $scope.img_rol = Constantes.ruta_imagenes + '/' + questionnaireService.getImgRol();
+                RoleFactory.get({'id': $scope.rol}).$promise
+                    .then(function (inforol) {
+                        questionnaireService.setImgRol(inforol.imagen);
+                        $scope.img_rol = Constantes.ruta_imagenes + '/' + questionnaireService.getImgRol();
 
-        if (inforol.tipo_rol === 'BC' || inforol.tipo_rol === 'BC') {
-          questionnaireService.setTipo('P');
-        } else{
-          questionnaireService.setTipo('S');
-        };
-        console.log(questionnaireService.getTipo());
-      }).catch(function(errors){
-        console.log("Error al recuperar los roles desde el servidor: ",errors);
-      }).finally(function(){
-        
-      });
+                        if (inforol.tipo_rol === 'BC' || inforol.tipo_rol === 'BC') {
+                            questionnaireService.setTipo('P');
+                        } else {
+                            questionnaireService.setTipo('S');
+                        }
+                        ;
+                    }).catch(function (errors) {
+                    console.log("Error al recuperar los roles desde el servidor: ", errors);
+                }).finally(function () {
 
-      $scope.ruta = Constantes.ruta_imagenes + "botones/";
-      $scope.perfil= $scope.ruta + "boton_registrate.png";    
-      $scope.conectar= $scope.ruta + "boton-conectarse.png";    
-      $scope.buscar= $scope.ruta + "boton_agregar_busqueda.png";    
-      $scope.inicio= $scope.ruta + "boton-comenzar.png";          
-      solutionService.setLogged('NOT');
+                });
 
-      $scope.menu_bar = function (view){
-        questionnaireService.changeView(view);                      
-      }
-      
-      $scope.profile = function(){
-        $location.path('/profileUpdate');
-      } 
+                solutionService.setLogged('NOT');
 
-      $scope.conexiones = function(){
-        $location.path('/profileConections');
-      } 
-
-      $scope.busquedas = function(){
-        $location.path('/profileSearch');
-      } 
-
-      $scope.new_b = function(){
-        $location.path('/questionnaires');
-      } 
-
-		}
-	]);
+                $scope.menu_bar = function (view) {
+                    questionnaireService.changeView(view);
+                }
+            }
+        ]);
 })();
--- public/js/controllers/sign-in-out/como-funciona-controller.js
+++ public/js/controllers/sign-in-out/como-funciona-controller.js
@@ -1,78 +1,61 @@
 /**
-* UserAdminController Module
-*
-* to redirect to edit user, create problems, complete social networks
-*/
+ * UserAdminController Module
+ *
+ * to redirect to edit user, create problems, complete social networks
+ */
 angular.module('gemStore')
-.controller('ComoFuncionaController', ['$scope', 'registroService', 'UserFactory', 'Constantes','$sce',
-        function($scope,registroService,UserFactory, Constantes,$sce){
-        //Inicio Rutas de imagenes
-        $scope.ruta= Constantes.ruta_imagenes + "botones/";    
-        tipo_app = Constantes.app;
-        if (tipo_app === 'C') {
-            $scope.logo= $scope.ruta + "logo-mercatic.png";    
-        } else{
-            $scope.logo= $scope.ruta + "logo-turistic.png";      
-        };        
-        $scope.config = {
-            preload: "none",
-            sources: [
-                {src: $sce.trustAsResourceUrl("videos/como_funciona.mp4"), type: "video/mp4"}
-                // {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-                // {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
-            ],
-            tracks: [
-                {
-                    src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-                    kind: "subtitles",
-                    srclang: "en",
-                    label: "English",
-                    default: ""
-                }
-            ],
-            theme: {
-                url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+    .controller('ComoFuncionaController', ['$scope', 'registroService', 'UserFactory', 'Constantes', '$sce',
+        function ($scope, registroService, UserFactory, Constantes, $sce) {
+            //Inicio Rutas de imagenes
+            $scope.ruta = Constantes.ruta_imagenes + "botones/";
+            tipo_app = Constantes.app;
+            if (tipo_app === 'C') {
+                $scope.logo = $scope.ruta + "logo-mercatic.png";
+            } else {
+                $scope.logo = $scope.ruta + "logo-turistic.png";
             }
-        };
+            ;
+            $scope.config = {
+                preload: "none",
+                sources: [
+                    {
+                        src: $sce.trustAsResourceUrl("videos/como_funciona.mp4"), type: "video/mp4"
+                    }
+                ],
+                tracks: [
+                    {
+                        src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
+                        kind: "subtitles",
+                        srclang: "en",
+                        label: "English",
+                        default: ""
+                    }
+                ],
+                theme: {
+                    url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+                }
+            };
 
-        $scope.iniciar_sesion= $scope.ruta + "boton_iniciar_sesion.png";    
-        $scope.registro= $scope.ruta + "boton_registrate.png";    
-        $scope.comenzar= $scope.ruta + "icono-comenzar.png";    
-        $scope.como_funciona= $scope.ruta + "boton_como_funciona.png";    
-        //Fin Rutas de imagenes
-        $scope.mensaje="";
-        $scope.login=function(){
-            registroService.changeView('auth');
-        }
-        $scope.signin=function(user)
-        {	
-            usuario = registroService.getUsuario();
-			console.log(usuario);
-            UserFactory.get({id:user}).$promise
-            .then(function(user){
-                registroService.setUsuario(user);
-                console.log(registroService.getUsuario());
-                registroService.changeView('personalData');
-           	
-            }).catch(function(errors){
-                $scope.mensaje="No se encontró al usuario";
-            }).finally(function(){
-                
-                
-            });
+            $scope.mensaje = "";
+            $scope.login = function () {
+                registroService.changeView('auth');
+            }
+            $scope.signin = function (user) {
+                usuario = registroService.getUsuario();
+                console.log(usuario);
+                UserFactory.get({id: user}).$promise
+                    .then(function (user) {
+                        registroService.setUsuario(user);
+                        console.log(registroService.getUsuario());
+                        registroService.changeView('personalData');
 
-        }	
-        
+                    }).catch(function (errors) {
+                    $scope.mensaje = "No se encontró al usuario";
+                }).finally(function () {
 
-        $scope.signup = function()
-        {
-            registroService.changeView('signin');
-        }
 
-        // Función para no registrado (Comienza a Explorar) 
-        $scope.noregistrado = function()
-        {
-            registroService.changeView('actionquestionnaire');
-        }
-	
-}]);
+                });
+
+            }
+
+        }]);
