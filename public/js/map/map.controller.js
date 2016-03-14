c935cf491ade5ef5779b46fb9375ad9fa23a3fc5
Victor Rodriguez <vrodmz@gmail.com>
2016-03-14 15:53:35 -0500

    Obteniendo lugares desde los servicios de google

--- public/js/map/map.controller.js
+++ public/js/map/map.controller.js
@@ -1,16 +1,43 @@
 'use strict';
 
 angular.module('map')
-    .controller('MapController', function ($scope, uiGmapGoogleMapApi) {
+    .controller('MapController', function ($scope, $timeout, uiGmapGoogleMapApi, uiGmapIsReady) {
         $scope.map = {
             center: {
                 latitude: 4.5809775,
                 longitude: -74.1340484
             },
+            control: {},
             zoom: 9
         };
 
-        uiGmapGoogleMapApi.then(function (maps) {
+        uiGmapGoogleMapApi.then(function (GMapsApi) {
+            //var placesService = new google.maps.places.PlacesService(GMapsApi.Map);
+            //console.log(placesService);
 
+            /*
+             var directionsRequest = {
+             origin: LatLng | String | google.maps.Place,
+             destination: LatLng | String | google.maps.Place,
+             }*/
+
+
+            //map.DirectionsService.route()
         });
+
+        uiGmapIsReady.promise().then(createPlacesService);
+
+        function createPlacesService() {
+            var mapObject = $scope.map.control.getGMap();
+            var placesService = new google.maps.places.PlacesService(mapObject);
+
+            var options = {
+                types: ['(cities)'],
+                componentRestrictions: {country: "co"}
+            };
+
+            var input = document.getElementById('from');
+            var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+        }
     });
--- public/js/map/map.js
+++ public/js/map/map.js
@@ -5,7 +5,7 @@ angular.module('map', ['uiGmapgoogle-maps'])
         uiGmapGoogleMapApiProvider.configure({
             key: 'AIzaSyB01Crhc-gyfYhAxa8IdSVcKuUC4lHai7E',
             v: '3.22',
-            libraries: 'weather,geometry,visualization'
+            libraries: 'weather,geometry,visualization,places'
         });
 
         $routeProvider
