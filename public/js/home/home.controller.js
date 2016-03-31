bf23c9209f4ce0978a4ea0d995dad3e34e1d2f34
AndresDiaz1 <desarrollo2@iMac-de-Vivelab.local>
2016-03-31 16:56:42 -0500

    cambios de la caja de buscador

--- public/js/home/home.controller.js
+++ public/js/home/home.controller.js
@@ -2,7 +2,14 @@
 
 angular.module('home',[])
     .controller('HomeController', function ($scope) {
+        $scope.mostrar=false;
+
         $scope.search =function(){
             console.log("hizo click en buscar");
+            $scope.mostrar=!$scope.mostrar;
+        }
+
+        $scope.makeSearch =function(){
+            console.log("presiono en realizar búsqueda");
         }
     });
