bdf1e3021db536748832a1d3ded8e560dd1ec280
AndresDiaz1 <desarrollo2@iMac-de-Vivelab.local>
2016-04-01 10:15:23 -0500

    Se selecciona municipio

--- public/js/home/home.controller.js
+++ public/js/home/home.controller.js
@@ -1,15 +1,25 @@
 'use strict';
 
 angular.module('home',[])
-    .controller('HomeController', function ($scope) {
-        $scope.mostrar=false;
+    .controller('HomeController', function ($scope, MunicipiosFactory) {
+        $scope.showExpandableContent=false;
 
         $scope.search =function(){
             console.log("hizo click en buscar");
-            $scope.mostrar=!$scope.mostrar;
+            $scope.showExpandableContent=!$scope.showExpandableContent;
         }
 
         $scope.makeSearch =function(){
             console.log("presiono en realizar búsqueda");
         }
+
+        $scope.selectedTown = function(index){
+            console.log("El municipio elegido fue", $scope.municipios[index].nombre);
+        }
+
+        MunicipiosFactory.query().$promise.then(function (response) {
+            $scope.municipios = response;
+        }).catch(function (error) {
+            console.log("Ocurrio un error", error);
+        });
     });
