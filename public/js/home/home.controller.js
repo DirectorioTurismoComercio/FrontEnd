13c823032eb128cb91ab9e542738c6ab3b6c585f
AndresDiaz1 <andiazvar@gmail.com>
2016-04-03 16:52:20 -0500

    Refactor del buscador

--- public/js/home/home.controller.js
+++ public/js/home/home.controller.js
@@ -1,21 +1,17 @@
 'use strict';
 
 angular.module('home')
-    .controller('HomeController', function ($scope, MunicipiosFactory, ResultRetriever, SearchForResultsFactory) {
-        $scope.showExpandableContent=false;
-        $scope.results =null;
+    .controller('HomeController', function ($scope, MunicipiosFactory, ResultRetriever, SearchForResultsFactory, $location) {
+        $scope.showExpandableContent = false;
+        $scope.results = null;
         $scope.serverResults = [];
 
-        $scope.showSearchForm=function(){
+        $scope.showSearchForm = function () {
             console.log("hizo click en buscar");
-            $scope.showExpandableContent=!$scope.showExpandableContent;
+            $scope.showExpandableContent = !$scope.showExpandableContent;
         }
 
-        $scope.makeSearch =function(){
-            console.log("presiono en realizar búsqueda");
-        }
-
-        $scope.selectedTown = function(index){
+        $scope.selectedTown = function (index) {
             console.log("El municipio elegido fue", $scope.municipios[index].nombre);
         }
 
@@ -23,27 +19,20 @@ angular.module('home')
             $scope.newresults = ResultRetriever.getresults(typedthings, "SuggestionsFactory");
             $scope.newresults.then(function (data) {
                 $scope.results = data;
-            }).catch(function(error){
+            }).catch(function (error) {
                 console.log("ocurrio un error", error);
             });
         }
 
         $scope.doSearch = function (result) {
-            SearchForResultsFactory.query({search: result},
-                function (data) {
-                    $scope.serverResults = data;
-                }
-            );
-            console.log("Los resultados fueron ", JSON.stringify($scope.serverResults));
+            SearchForResultsFactory.doSearch(result).then(function(response){
+                $location.path('/map');
+                console.log("Los resultados en el controlador home son", SearchForResultsFactory.getResults());
+            }).catch(function(error){
+                console.log("ocurrio un error", error);
+            });
         }
 
-        $scope.doSearchBySuggestion = function (suggestion) {
-            SearchForResultsFactory.query({search: suggestion},
-                function (data) {
-                    $scope.serverResults = data;
-                }
-            );
-        };
 
         MunicipiosFactory.query().$promise.then(function (response) {
             $scope.municipios = response;
--- public/js/services/search-for-results.js
+++ public/js/services/search-for-results.js
@@ -1,7 +1,25 @@
+(function () {
+    angular.module('gemStore')
+        .factory('SearchForResultsFactory', ['$resource', 'API_CONFIG', function ($resource, API_CONFIG) {
 
-(function(){
-	angular.module('gemStore')
-		.factory('SearchForResultsFactory', ['$resource', 'API_CONFIG',function($resource,API_CONFIG) {
-			return $resource(API_CONFIG.url+'/buscar',{search: '@search'});
-		}]);
+            var serverResults;
+
+            return {
+                doSearch: doSearch,
+                getResults: getResults
+            };
+
+            function doSearch(result) {
+                return $resource(API_CONFIG.url + '/buscar', {search: '@search'}).query({search: result},
+                    function (data) {
+                        serverResults = data;
+                    }
+                ).$promise;
+            }
+
+            function getResults() {
+                return serverResults;
+            }
+
+        }]);
 })();
