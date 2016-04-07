86068b63aba4776c5263e425ee3caddb1c7a78c5
Ingeniero 1 <vivelabcrear@gmail.com>
2016-04-07 16:32:15 -0500

    Se corrige error en pruebas unitarias y se coloca sección tab de buscar y arma tu ruta

--- karma.conf.js
+++ karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function (config) {
             'public/js/map/map.controller.js',
             'public/js/home/home.js',
             'public/js/home/home.controller.js',
+            'public/js/businessBrowser/businessBrowser.controller.js',
             'public/js/**/*.js',
             'test/**/*Spec.js',
             'public/templates/directives/side-bar.html'
