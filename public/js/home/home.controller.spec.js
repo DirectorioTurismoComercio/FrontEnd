f9f4012cde4719ee945c0737b1b1d60a1bf5a801
Ingeniero1 <vivelabcrear@gmail.com>
2016-04-07 20:43:05 -0500

    prueba de unit test de promesas

--- public/js/home/home.controller.spec.js
+++ public/js/home/home.controller.spec.js
@@ -1,18 +1,41 @@
 'use strict';
 
 describe('Controller: HomeController', function () {
-    var homeController, scope;
+    var homeController, $scope, $qTest, deferred, location, mdDialog;
 
     beforeEach(module('gemStore'));
 
-    beforeEach(inject(function ($controller, $rootScope) {
-        scope = $rootScope.$new();
+    beforeEach(inject(function ($controller, $rootScope, $q, SearchForResultsFactory, $location, $mdDialog) {
+        $scope = $rootScope.$new();
+        $qTest=$q;
+        deferred=$q.defer();
+        mdDialog=$mdDialog;
+
+        spyOn(SearchForResultsFactory,'doSearch').and.returnValue(deferred.promise);
+        spyOn(mdDialog, 'show');
+
         homeController = $controller('HomeController', {
-            $scope: scope
+            $scope: $scope,
+            SearchForResultsFactory: SearchForResultsFactory,
+            $mdDialog:mdDialog
         });
     }));
 
     it('Search callback should exist', function () {
-        expect(scope.doSearch).toBeDefined();
+        expect($scope.doSearch).toBeDefined();
     });
+/*
+    it('Should show error message if search has zero results', function () {
+        deferred.resolve([{}]);
+        $scope.$apply();
+        $scope.doSearch();
+        expect(mdDialog.show).toHaveBeenCalled();
+    });
+    it('Should show error message if has ocurred an error', function () {
+        deferred.reject();
+        $scope.$apply();
+        $scope.doSearch();
+        expect(console.log).toHaveBeenCalled();
+    });
+*/
 });
