332cd657b517e1c67557ce4f07123970c0ef1f3e
Ingeniero 1 <vivelabcrear@gmail.com>
2016-04-08 17:02:47 -0500

    Prueba unitaria del home controller

--- public/js/home/home.controller.spec.js
+++ public/js/home/home.controller.spec.js
@@ -1,41 +1,54 @@
 'use strict';
 
 describe('Controller: HomeController', function () {
-    var homeController, $scope, $qTest, deferred, location, mdDialog;
+    var homeController, $scope, $qTest, deferred, location, mdDialog, searchFactory, mockSearch;
 
     beforeEach(module('gemStore'));
-
+    beforeEach(module('home'));
+    
     beforeEach(inject(function ($controller, $rootScope, $q, SearchForResultsFactory, $location, $mdDialog) {
         $scope = $rootScope.$new();
-        $qTest=$q;
-        deferred=$q.defer();
-        mdDialog=$mdDialog;
+        $qTest = $q;
 
-        spyOn(SearchForResultsFactory,'doSearch').and.returnValue(deferred.promise);
+        mdDialog = $mdDialog;
+
+        searchFactory = SearchForResultsFactory;
+
+        mockSearch =
+        {
+            doSearch:function(){
+                deferred = $q.defer();
+                return deferred.promise
+            }
+        }
+
+
+        spyOn(SearchForResultsFactory, 'doSearch').and.returnValue();
         spyOn(mdDialog, 'show');
 
         homeController = $controller('HomeController', {
             $scope: $scope,
-            SearchForResultsFactory: SearchForResultsFactory,
-            $mdDialog:mdDialog
+            SearchForResultsFactory: mockSearch,
+            $mdDialog: mdDialog
         });
     }));
 
     it('Search callback should exist', function () {
         expect($scope.doSearch).toBeDefined();
     });
-/*
+
     it('Should show error message if search has zero results', function () {
-        deferred.resolve([{}]);
-        $scope.$apply();
         $scope.doSearch();
+        deferred.resolve([])
+        $scope.$apply();
         expect(mdDialog.show).toHaveBeenCalled();
     });
-    it('Should show error message if has ocurred an error', function () {
-        deferred.reject();
-        $scope.$apply();
-        $scope.doSearch();
-        expect(console.log).toHaveBeenCalled();
-    });
-*/
+    /*
+     it('Should show error message if has ocurred an error', function () {
+     deferred.reject();
+     $scope.$apply();
+     $scope.doSearch();
+     expect(console.log).toHaveBeenCalled();
+     });
+     */
 });
