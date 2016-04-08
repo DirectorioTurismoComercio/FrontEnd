4b0f39720321d598da75574b4c1979870c4a94ed
Ingeniero 1 <vivelabcrear@gmail.com>
2016-04-08 17:16:19 -0500

    Pruebas unitarias del home

--- public/js/home/home.controller.spec.js
+++ public/js/home/home.controller.spec.js
@@ -1,30 +1,29 @@
 'use strict';
 
 describe('Controller: HomeController', function () {
-    var homeController, $scope, $qTest, deferred, mdDialog;
+    var homeController, $scope, $qTest, deferred, mdDialog, location;
 
     beforeEach(module('gemStore'));
     beforeEach(module('home'));
 
-    beforeEach(inject(function ($controller, $rootScope, $q, SearchForResultsFactory, $mdDialog) {
+    beforeEach(inject(function ($controller, $rootScope, $q, SearchForResultsFactory, $mdDialog, $location) {
         $scope = $rootScope.$new();
         deferred = $q.defer();
         $qTest = $q;
         mdDialog = $mdDialog;
+        location = $location;
 
         spyOn(SearchForResultsFactory, 'doSearch').and.returnValue(deferred.promise);
         spyOn(mdDialog, 'show');
+        spyOn(location, 'path');
 
         homeController = $controller('HomeController', {
             $scope: $scope,
             SearchForResultsFactory: SearchForResultsFactory,
+            $location: location,
             $mdDialog: mdDialog
         });
     }));
-
-    it('Search callback should exist', function () {
-        expect($scope.doSearch).toBeDefined();
-    });
 
     it('Should show error message if search has zero results', function () {
         $scope.doSearch();
@@ -32,5 +31,12 @@ describe('Controller: HomeController', function () {
         $scope.$apply();
         expect(mdDialog.show).toHaveBeenCalled();
     });
-    
+
+    it('Should redirects to map page', function () {
+        $scope.doSearch();
+        deferred.resolve(['Repuesta']);
+        $scope.$apply();
+        expect(location.path).toHaveBeenCalled();
+    });
+
 });
