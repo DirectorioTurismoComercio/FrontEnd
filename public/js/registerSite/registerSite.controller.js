2e374d4e754255243c0de000ca674580049f588d
Ingeniero 1 <vivelabcrear@gmail.com>
2016-05-04 10:41:19 -0500

    Se incluyen validaciones en el registro

--- public/js/registerSite/registerSite.controller.js
+++ public/js/registerSite/registerSite.controller.js
@@ -9,7 +9,6 @@ angular.module('registerSite')
         $scope.businessDescription='';
         $scope.tags='';
         $scope.businessEmail='';
-        
 
         $scope.map = {
             center: {latitude: 4.6363623, longitude: -74.0854427}, control: {}, zoom: 9,
@@ -33,16 +32,15 @@ angular.module('registerSite')
 
 
         $scope.register=function(){
-
+            doPhoneOnlyNumbersValidation();
+            doEmailPatternValidation();
             doFieldValidation($scope.sitePhoneNumber, "Por favor ingrese un número teléfonico, o verifique el número ingresado");
-            doPhoneValidation();
             doFieldValidation($scope.openingHours, "Por favor ingrese un horario de atención");
             doFieldValidation($scope.businessName, "Por favor ingrese el nombre del local");
             doFieldValidation($scope.businessLocation, "Por favor ubique su local haciendo click en el mapa");
             doFieldValidation($scope.businessDescription, "Por favor ingrese una descripción de su local");
             doFieldValidation($scope.tags, "Por favor ingrese almenos un tag");
             doFieldValidation($scope.businessEmail, "Por favor ingrese un correo electrónico");
-
         }
 
         function doFieldValidation(field,errorMessage){
@@ -51,9 +49,15 @@ angular.module('registerSite')
             }
         }
 
-        function doPhoneValidation(){
+        function doPhoneOnlyNumbersValidation(){
             if(!/^\d+$/.test($scope.sitePhoneNumber)){
                 popErrorAlertService.showPopErrorAlert("Por favor, verifique el número ingresado")
+            }
+        }
+
+        function doEmailPatternValidation(){
+            if(!/^.+@.+\..+$/.test($scope.businessEmail)){
+                popErrorAlertService.showPopErrorAlert("Por favor, verifique el email ingresado")
             }
         }
 
