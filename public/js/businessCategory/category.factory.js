be3cd0c1f1ecaa0d02f23640f4043d7252694b0e
Ingeniero1 <vivelabcrear@gmail.com>
2016-05-07 13:45:58 -0500

    Se cargan las categorías y subcategorias del back end

--- public/js/businessCategory/category.factory.js
+++ public/js/businessCategory/category.factory.js
@@ -1,39 +1,16 @@
 'use strict';
 
 angular.module('businessCategory', [])
-    .factory('categories',function ($filter) {
-        /*VARIABLES DE EJEMPLO HASTA QUE SE IMPLEMENTE CATEGORIAS EN BACKEND*/
+    .factory('categories',function ($filter, $resource, API_CONFIG) {
         var service = {};
 
-
-        var categorylist = [
-            { "id": 1, "category": "Restaurantes" },
-            { "id": 2, "category": "Tiendas" },
-            { "id": 3, "category": "Entretenimiento" },
-        ];
-
-        var subcategorylist = [
-            {"Id":1, "subcategory":"Comida Rápida", "categoryID": 1},
-            {"Id":2, "subcategory":"Económico", "categoryID": 1},
-            {"Id":3, "subcategory":"Buffet", "categoryID": 1},
-            {"Id":4, "subcategory":"Viveres", "categoryID": 2},
-            {"Id":5, "subcategory":"Ropa", "categoryID": 2},
-            {"Id":6, "subcategory":"Cine", "categoryID": 3},
-            {"Id":7, "subcategory":"Centro Comercial", "categoryID": 3},
-            {"Id":8, "subcategory":"Música", "categoryID": 3}
-        ];
-
-
         service.getCategories = function(){
-            return categorylist;
+            return $resource(API_CONFIG.url+API_CONFIG.categorias+'?nivel=1').query().$promise;
         };
 
-        service.getSubcategories = function(categoryID){
-            var subcategories = ($filter('filter')(subcategorylist, {categoryID: categoryID}));
-            return subcategories;
+        service.getSubcategories = function(parentCategoryId){
+            return $resource(API_CONFIG.url+API_CONFIG.categorias+'?nivel=2&categoria_padre='+parentCategoryId).query().$promise;
         };
-
 
         return service;
-
     });
--- public/js/registerSite/registerSite.controller.js
+++ public/js/registerSite/registerSite.controller.js
@@ -4,15 +4,15 @@ angular.module('registerSite')
     .controller('registerSiteController', function ($scope, $http, MapService, uiGmapIsReady, popErrorAlertService, CUNDINAMARCA_COORDS,
                                                     BOGOTA_COORDS, categories) {
 
-        $scope.sitePhoneNumber=undefined;
-        $scope.openingHours=undefined;
-        $scope.businessLocation=undefined;
-        $scope.businessDescription=undefined;
-        $scope.tags=undefined;
-        $scope.businessEmail=undefined;
+        $scope.sitePhoneNumber = undefined;
+        $scope.openingHours = undefined;
+        $scope.businessLocation = undefined;
+        $scope.businessDescription = undefined;
+        $scope.tags = undefined;
+        $scope.businessEmail = undefined;
 
-        $scope.businessCategories ={
-            category:'',
+        $scope.businessCategories = {
+            category: '',
             subcategory: '',
         };
 
@@ -28,29 +28,44 @@ angular.module('registerSite')
             }
         };
 
-        $scope.categories = categories.getCategories();
-
-        $scope.getCountryStates = function(){
-            if($scope.businessCategories.category==undefined){
-                $scope.subcategories=undefined;
-                $scope.businessCategories.subcategory=undefined;
-            }else{
-                $scope.subcategories = categories.getSubcategories($scope.businessCategories.category);
-            }
-        }
-
-
 
         uiGmapIsReady.promise().then(function (map_instances) {
             MapService.setGMap(map_instances[0].map);
         });
 
-        $scope.register=function(){
+        categories.getCategories().then(function (response) {
+            $scope.categories = response;
+        }).catch(function (error) {
+            console.log("Hubo un error", error);
+        });
+
+        $scope.getControllerSubcategories = function () {
+            if ($scope.businessCategories.category == undefined) {
+                resetCategoriesModel();
+            } else {
+                getSubcategories();
+            }
+        }
+
+        $scope.register = function () {
             alert("presiono registrarse");
         }
-        
 
-        function getClickedPositionCoordinates(originalEventArgs){
+        function resetCategoriesModel(){
+            $scope.subcategories = undefined;
+            $scope.businessCategories.subcategory = undefined;
+        }
+
+        function getSubcategories(){
+            categories.getSubcategories($scope.businessCategories.category).then(function (response) {
+                $scope.subcategories = response;
+            }).catch(function (error) {
+                console.log("hubo un error", error);
+            });
+        }
+
+
+        function getClickedPositionCoordinates(originalEventArgs) {
             var e = originalEventArgs[0];
             $scope.businessLocation = {
                 lat: e.latLng.lat(),
@@ -58,26 +73,26 @@ angular.module('registerSite')
             };
         }
 
-        function drawMarkerIfIsInsideBoundaries(){
-            var isBusinessInsideCundinamarca=MapService.isPlaceInsideBoundaries($scope.businessLocation.lat,$scope.businessLocation.lng, CUNDINAMARCA_COORDS);
-            var isBusinessInsideBogota=MapService.isPlaceInsideBoundaries($scope.businessLocation.lat,$scope.businessLocation.lng, BOGOTA_COORDS);
+        function drawMarkerIfIsInsideBoundaries() {
+            var isBusinessInsideCundinamarca = MapService.isPlaceInsideBoundaries($scope.businessLocation.lat, $scope.businessLocation.lng, CUNDINAMARCA_COORDS);
+            var isBusinessInsideBogota = MapService.isPlaceInsideBoundaries($scope.businessLocation.lat, $scope.businessLocation.lng, BOGOTA_COORDS);
 
-            if(!isBusinessInsideCundinamarca){
+            if (!isBusinessInsideCundinamarca) {
                 displayOutsideBoundaryErrorMessage("La ubicación del local está fuera de Cundinamarca");
             }
 
-            if(isBusinessInsideBogota){
+            if (isBusinessInsideBogota) {
                 displayOutsideBoundaryErrorMessage("La ubicación del local está dentro de Bogotá");
             }
 
-            if(isBusinessInsideCundinamarca && !isBusinessInsideBogota){
+            if (isBusinessInsideCundinamarca && !isBusinessInsideBogota) {
                 MapService.addMarker($scope.businessLocation, "pepe");
             }
         }
 
-        function displayOutsideBoundaryErrorMessage(message){
+        function displayOutsideBoundaryErrorMessage(message) {
             popErrorAlertService.showPopErrorAlert(message);
-            $scope.businessLocation=undefined;
+            $scope.businessLocation = undefined;
         }
 
         $scope.files = null;
@@ -88,34 +103,33 @@ angular.module('registerSite')
         }
 
 
-
         $scope.upload = function () {
             console.log("el contenido de files", $scope.files);
 
-            var categorias=[1,2];
+            var categorias = [1, 2];
 
 
-            var fd=new FormData();
-            var i=0;
-            angular.forEach($scope.files,function(file) {
-                fd.append('foto'+i,file);
+            var fd = new FormData();
+            var i = 0;
+            angular.forEach($scope.files, function (file) {
+                fd.append('foto' + i, file);
                 i++;
             });
 
-            fd.append('latitud',-74.12);
-            fd.append('longitud',4.23);
-            fd.append('nombre','pepillo12');
-            fd.append('descripcion','bla bla');
-            fd.append('categorias',1);
-            fd.append('categorias',4);
+            fd.append('latitud', -74.12);
+            fd.append('longitud', 4.23);
+            fd.append('nombre', 'pepillo12');
+            fd.append('descripcion', 'bla bla');
+            fd.append('categorias', 1);
+            fd.append('categorias', 4);
 
 
-            console.log("el fd",fd);
+            console.log("el fd", fd);
 
             $http.post('http://ecosistema.desarrollo.com:8000/sitio', fd,
                 {
-                    transformRequest:angular.identity,
-                    headers: {'Content-Type':undefined}
+                    transformRequest: angular.identity,
+                    headers: {'Content-Type': undefined}
                 })
                 .success(function (d) {
                     console.log("la respyesat de django", d);
