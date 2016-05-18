dddca2bdbfcef02ba802902d15e431c727472cca
Ingeniero 1 <vivelabcrear@gmail.com>
2016-05-18 11:35:45 -0500

    refactor código innecesario en el placeholder del input box del buscador

--- public/js/businessBrowser/businessBrowser.controller.js
+++ public/js/businessBrowser/businessBrowser.controller.js
@@ -1,10 +1,10 @@
 angular.module('businessBrowser', [])
     .controller('businessBrowserController', function ($scope, ResultRetriever, siteAndTownSaverService) {
 
-        setPlaceholderAndValue();
-
+        setValue();
 
         $scope.lookForSuggestions = function (typedthings) {
+            console.log("entro a sugerencias");
             $scope.newresults = ResultRetriever.getresults(typedthings, "SuggestionsFactory");
             $scope.newresults.then(function (data) {
                 $scope.results = data;
@@ -13,8 +13,7 @@ angular.module('businessBrowser', [])
             });
         }
 
-        function setPlaceholderAndValue() {
-            $scope.browserBusinessPlaceholder = (siteAndTownSaverService.getCurrentSearchedSite() == null) ? '¿Qué Buscas?' : siteAndTownSaverService.getCurrentSearchedSite();
+        function setValue() {
             $scope.result= (siteAndTownSaverService.getCurrentSearchedSite() == null) ? undefined : siteAndTownSaverService.getCurrentSearchedSite();
         }
     });
