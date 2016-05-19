23d684d5b71c5bb558849a96ec0f2543e7a826d2
Estudiante Auxiliar <vivelabcrear@gmail.com>
2016-05-19 16:07:32 -0500

    Archivo constantes excluido de los commits

---
+++ public/js/constants.js
@@ -0,0 +1,11 @@
+angular.module('constants', [])
+    .constant("API_CONFIG", {
+        "url": "http://127.0.0.1:8000",
+        "login": "/rest-auth/login",
+        "user": "/usuario",
+        "logout": "/rest-auth/login",
+        "facebookID": "822335427898991",
+        "googleID": "348524611866-dkkhnm8pk6vudvppd8tc52te3gr6d510.apps.googleusercontent.com",
+        "sitios": "/ruta/sitios",
+        "categorias": "/categorias"
+    });
