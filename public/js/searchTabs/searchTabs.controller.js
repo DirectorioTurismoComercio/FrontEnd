4a7515068417ebf4bc712bfabf8721f6232a950b
Estudiante Auxiliar <vivelabcrear@gmail.com>
2016-05-24 17:01:22 -0500

    Union de las directivas de las pestañas en una sola

--- public/js/searchTabs/searchTabs.controller.js
+++ public/js/searchTabs/searchTabs.controller.js
@@ -1,5 +1,7 @@
 angular.module('searchTabs', ['google.places', 'geolocation'])
-    .controller('searchTabsController', function ($scope, siteAndTownSaverService) {
+    .controller('searchTabsController', function ($scope, geolocation, popErrorAlertService,
+                                                  siteAndTownSaverService, CUNDINAMARCA_COORDS,
+                                                  MapService) {
         $scope.isSearchFormVisible = false;
         $scope.isRouteFormVisible = false;
         showSearchFormIfUserHasMadeASearch();
@@ -27,6 +29,64 @@ angular.module('searchTabs', ['google.places', 'geolocation'])
             if (siteAndTownSaverService.getSearchedRoute() != undefined) {
                 $scope.isRouteFormVisible = true;
                 siteAndTownSaverService.resetSearch();
+            }
+        }
+
+
+        var cundinamarcaPolygon = new google.maps.Polygon({paths: CUNDINAMARCA_COORDS});
+        $scope.loadingCurrentPosition = false;
+        $scope.autocompleteOptions = {
+            componentRestrictions: {country: 'co'},
+        };
+
+        if (siteAndTownSaverService.getOrigin() != undefined) {
+            setSearchedRoutePlaceHolders();
+        }
+
+        $scope.deleteText = function (model) {
+            clearText(model);
+        };
+
+        $scope.searchFieldIsFill = function (latitude, longitude, model) {
+            if (latitude != undefined || longitude != undefined) {
+                serachIfSiteIsInCundinamarca(latitude, longitude, model);
+            }
+        };
+
+        $scope.getUserPosition = function () {
+            $scope.loadingCurrentPosition = true;
+            geolocation.getLocation().then(function (data) {
+                var coords = MapService.coordsToLatLng(data.coords.latitude, data.coords.longitude);
+                $scope.routeToController.routeFrom = "Mi posición actual";
+                siteAndTownSaverService.setOrigin(coords);
+                $scope.loadingCurrentPosition = false;
+            }).catch(function (error) {
+                $scope.loadingCurrentPosition = false;
+                popErrorAlertService.showPopErrorAlert("No es posible obtener la ubicación");
+            });
+        };
+
+        function setSearchedRoutePlaceHolders() {
+            (typeof siteAndTownSaverService.getOrigin().lat === "function") ?
+                $scope.routeToController.routeFrom = siteAndTownSaverService.getCurrentOriginPlaceName() : $scope.routeToController.routeFrom = "Mi posición actual";
+            $scope.routeToController.routeTo = siteAndTownSaverService.getCurrentDestinationPlaceName();
+        }
+
+        function serachIfSiteIsInCundinamarca(latitude, longitude, model) {
+            var coords = new google.maps.LatLng(latitude, longitude);
+            cundinamarcaPolygon = new google.maps.Polygon({paths: CUNDINAMARCA_COORDS});
+            if (!google.maps.geometry.poly.containsLocation(coords, cundinamarcaPolygon)) {
+                popErrorAlertService.showPopErrorAlert("El lugar seleccionado no esta disponible por el momento");
+                clearText(model);
+            }
+        }
+
+        function clearText(model) {
+            if (model == 'routeToController.routeFrom') {
+                $scope.routeToController.routeFrom = ''
+            }
+            if (model == 'routeToController.routeTo') {
+                $scope.routeToController.routeTo = ''
             }
         }
     });
