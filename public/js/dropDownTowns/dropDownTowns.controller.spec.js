09238f72767c6685cef57b3cdbc949db5874c747
Ingeniero 1 <vivelabcrear@gmail.com>
2016-05-31 18:25:36 -0500

    pruebas unitarias del bug no se traduce Todo cundinamarca de home a map y viceversa

--- public/js/dropDownTowns/dropDownTowns.controller.spec.js
+++ public/js/dropDownTowns/dropDownTowns.controller.spec.js
@@ -1,8 +1,21 @@
 'use strict';
 
 describe('Controller: dropDownTownsController', function () {
-    var dropDownTownsController, $scope, deferred, testsiteAndTownSaverService;
-
+    var dropDownTownsController, $scope, deferred, testsiteAndTownSaverService, testtranslate;
+    var response=[
+        {
+            "id": 1,
+            "nombre": "Bogotá",
+            "latitud": "4.667715085742610000",
+            "longitud": "-74.054495454505900000"
+        },
+        {
+            "id": 2,
+            "nombre": "Boita",
+            "latitud": "4.668280000000000000",
+            "longitud": "-74.055457000000000000"
+        }
+    ];
     beforeEach(module('gemStore'));
     beforeEach(module('dropDownTowns'));
 
@@ -20,20 +33,41 @@ describe('Controller: dropDownTownsController', function () {
 
     }));
 
-    beforeEach(inject(function ($controller, $rootScope, $q,siteAndTownSaverService, MunicipiosFactory) {
+    beforeEach(inject(function ($controller, $rootScope, $q,siteAndTownSaverService, MunicipiosFactory, $translate) {
         $scope = $rootScope.$new();
         deferred = $q.defer();
         testsiteAndTownSaverService=siteAndTownSaverService;
+        testtranslate=$translate;
 
         spyOn(MunicipiosFactory, 'getTowns').and.returnValue(deferred.promise);
+
         spyOn(testsiteAndTownSaverService, 'setCurrentSearchedTown');
 
         dropDownTownsController = $controller('dropDownTownsController', {
             $scope: $scope,
             MunicipiosFactory:MunicipiosFactory,
-            siteAndTownSaverService:testsiteAndTownSaverService
+            siteAndTownSaverService:testsiteAndTownSaverService,
+            $translate:testtranslate
         });
     }));
+
+    it('should set Todo Cundinamarca in first place if page is in spanish language', function(){
+        spyOn(testtranslate, 'use').and.returnValue('es');
+        $scope.isonregistersite=false;
+        deferred.resolve(response);
+        $scope.$apply();
+        $scope.municipios=response;
+        expect($scope.municipios[0].nombre).toBe('Todo Cundinamarca');
+    });
+
+    it('should set All Cundinamarca in first place if page is in english language', function(){
+        spyOn(testtranslate, 'use').and.returnValue('en');
+        $scope.isonregistersite=false;
+        deferred.resolve(response);
+        $scope.$apply();
+        $scope.municipios=response;
+        expect($scope.municipios[0].nombre).toBe('All Cundinamarca');
+    });
 
     it('Should set current searched town', function () {
         deferred.resolve([
