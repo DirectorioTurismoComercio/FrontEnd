772fabd0d7ee06abe5580fc74bcefa3590ec66e4
Ingeniero 1 <vivelabcrear@gmail.com>
2016-06-03 17:00:30 -0500

    Se crea un servicio que almacena los datos del local

--- public/js/registerSite/registerSite.controller.js
+++ public/js/registerSite/registerSite.controller.js
@@ -3,21 +3,22 @@
 angular.module('registerSite')
     .controller('registerSiteController', function ($scope, $http, MapService, uiGmapIsReady, messageService, CUNDINAMARCA_COORDS,
                                                     BOGOTA_COORDS, API_CONFIG, categories,
-                                                    $location,authenticationService, siteAndTownSaverService, $translate,geolocation) {
+                                                    $location, authenticationService, siteAndTownSaverService, siteInformationService, $translate, geolocation) {
 
-        $scope.sitePhoneNumber = undefined;
-        $scope.whatsapp = undefined;
-        $scope.openingHours = undefined;
-        $scope.businessName = undefined;
-        $scope.businessLocation = undefined;
-        $scope.businessDescription = undefined;
-        $scope.tags = undefined;
-        $scope.businessEmail = undefined;
-        $scope.businessAddress=undefined;
+        $scope.sitePhoneNumber = siteInformationService.sitePhoneNumber;
+        $scope.whatsapp = siteInformationService.whatsapp;
+        $scope.openingHours = siteInformationService.openingHours;
+        $scope.businessName = siteInformationService.businessName;
+        $scope.businessLocation = siteInformationService.businessLocation;
+        $scope.businessDescription = siteInformationService.businessDescription;
+        $scope.tags = siteInformationService.tags;
+        $scope.businessEmail = siteInformationService.businessEmail;
+        $scope.businessAddress = siteInformationService.businessAddress;
         $scope.files = undefined;
         $scope.businessCategories = {
             category: ''
         };
+        
         $scope.map = {
             center: {latitude: 4.6363623, longitude: -74.0854427}, control: {}, zoom: 9,
             events: {
@@ -44,8 +45,8 @@ angular.module('registerSite')
             MapService.setGMap(map_instances[0].map);
         });
 
-        $scope.subir=function(){
-            console.log("El arreglo del flow",$scope.flowPhotos);
+        $scope.subir = function () {
+            console.log("El arreglo del flow", $scope.flowPhotos);
         }
 
         $scope.filesChange = function (elm) {
@@ -53,7 +54,7 @@ angular.module('registerSite')
             $scope.$apply();
         };
 
-        $scope.getUserPosition=function(){
+        $scope.getUserPosition = function () {
             geolocation.getLocation().then(function (data) {
                 var userPosition = MapService.coordsToLatLngLiteral(data.coords.latitude, data.coords.longitude);
                 MapService.setPinOnUserPosition(userPosition);
@@ -67,8 +68,6 @@ angular.module('registerSite')
         $scope.register = function () {
 
 
-
-
             var fd = new FormData();
             var i = 0;
             angular.forEach($scope.files, function (file) {
@@ -80,31 +79,35 @@ angular.module('registerSite')
             fd.append('longitud', $scope.businessLocation.lng);
             fd.append('nombre', $scope.businessName);
             fd.append('descripcion', $scope.businessDescription);
-            fd.append('municipio',siteAndTownSaverService.getCurrentSearchedTown().id);
-            fd.append('telefono',$scope.sitePhoneNumber);
-            fd.append('horariolocal',$scope.openingHours);
-            fd.append('correolocal',$scope.businessEmail);
+            fd.append('municipio', siteAndTownSaverService.getCurrentSearchedTown().id);
+            fd.append('telefono', $scope.sitePhoneNumber);
+            fd.append('horariolocal', $scope.openingHours);
+            fd.append('correolocal', $scope.businessEmail);
             fd.append('ubicacionlocal', $scope.businessAddress);
             fd.append('categorias', $scope.businessCategories.category);
-            fd.append('usuario',1);
-            for(var i=0; i<=$scope.tags.length-1;i++){
+            fd.append('usuario', 1);
+            for (var i = 0; i <= $scope.tags.length - 1; i++) {
                 fd.append('tags', $scope.tags[i].text);
             }
 
 
-
-            $http.post(API_CONFIG.url+API_CONFIG.sitio, fd,
+            $http.post(API_CONFIG.url + API_CONFIG.sitio, fd,
                 {
                     transformRequest: angular.identity,
                     headers: {'Content-Type': undefined}
                 })
                 .success(function (d) {
-                    messageService.showSuccessMessage("REGISTER_COMPLETE","SUCCESS_TITLE_MESSAGE");
+                    messageService.showSuccessMessage("REGISTER_COMPLETE", "SUCCESS_TITLE_MESSAGE");
                 }).error(function (error) {
                 console.log("hubo n error", error);
             });
             siteAndTownSaverService.setCurrentSearchedTown(undefined);
         };
+
+        $scope.changeView = function (view) {
+            saveSiteInformation();
+            $location.path(view);
+        }
 
         function getClickedPositionCoordinates(originalEventArgs) {
             var e = originalEventArgs[0];
@@ -115,19 +118,19 @@ angular.module('registerSite')
             getClickedPositionTown();
         }
 
-        function getClickedPositionTown(){
-            $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng='+$scope.businessLocation.lat+','+$scope.businessLocation.lng+'&sensor=true')
-                .success(function(response){
-                    joinOfFormatted_address=response.results[0].formatted_address+response.results[1].formatted_address;
+        function getClickedPositionTown() {
+            $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + $scope.businessLocation.lat + ',' + $scope.businessLocation.lng + '&sensor=true')
+                .success(function (response) {
+                    joinOfFormatted_address = response.results[0].formatted_address + response.results[1].formatted_address;
                     MapService.clearMarkers();
                     drawMarkerIfIsInsideBoundaries();
                 });
         }
 
         function drawMarkerIfIsInsideBoundaries() {
-            if(!joinOfFormatted_address.includes(siteAndTownSaverService.getCurrentSearchedTown().nombre)){
+            if (!joinOfFormatted_address.includes(siteAndTownSaverService.getCurrentSearchedTown().nombre)) {
                 displayOutsideBoundaryErrorMessage("Verifique que la ubicación se encuentre en el municipio seleccionado");
-            }else{
+            } else {
                 MapService.addMarker($scope.businessLocation, $scope.businessName);
             }
         }
@@ -137,8 +140,15 @@ angular.module('registerSite')
             $scope.businessLocation = undefined;
         }
 
-        $scope.changeView = function(view){
-            $location.path(view); 
+        function saveSiteInformation(){
+            siteInformationService.sitePhoneNumber = $scope.sitePhoneNumber;
+            siteInformationService.whatsapp = $scope.whatsapp;
+            siteInformationService.openingHours = $scope.openingHours;
+            siteInformationService.businessName = $scope.businessName;
+            siteInformationService.businessLocation = $scope.businessLocation;
+            siteInformationService.businessDescription = $scope.businessDescription;
+            siteInformationService.tags = $scope.tags;
+            siteInformationService.businessEmail = $scope.businessEmail;
+            siteInformationService.businessAddress = $scope.businessAddress;
         }
-
     });
