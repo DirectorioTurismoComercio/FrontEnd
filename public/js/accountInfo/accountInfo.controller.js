a1230730425f5cbb99aece43dbf77cc26002c4af
Ingeniero 1 <vivelabcrear@gmail.com>
2016-06-28 15:47:42 -0500

    valida que la contraseña nueva y la de confirmación sean las mismas

--- public/js/accountInfo/accountInfo.controller.js
+++ public/js/accountInfo/accountInfo.controller.js
@@ -2,11 +2,21 @@
 angular.module('accountInfo')
     .controller('AccountInfoController', function ($scope, $location,
                                                    authenticationService) {
+
+        $scope.showRequiredFieldMessage = false;
         $scope.usuario = authenticationService.getUser();
-        
+
         console.log("usuario", $scope.usuario);
 
-        $scope.addBusiness=function(){
+        $scope.save = function () {
+            if($scope.traderInfoForm.$valid){
+
+            }else{
+                $scope.showRequiredFieldMessage=true;
+            }
+        }
+
+        $scope.addBusiness = function () {
             $location.path('businessinformation');
         }
     });
