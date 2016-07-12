f364114b2fd999ded3980e3d1e46fb56619ad4d3
Ingeniero 1 <vivelabcrear@gmail.com>
2016-07-12 09:12:33 -0500

    Se colocan algunos textos de la pantalla de categorias

--- public/js/registerSite/category/category.controller.js
+++ public/js/registerSite/category/category.controller.js
@@ -2,26 +2,34 @@
 
 angular.module('registerSite')
     .controller('categoryController', function ($scope, $auth, $http, messageService,
-                                                           API_CONFIG, categories,
-                                                           $location, MunicipiosFactory, authenticationService, siteAndTownSaverService,
-                                                           siteInformationService, $translate, navigationService) {
+                                                API_CONFIG, categories,
+                                                $location, MunicipiosFactory, authenticationService, siteAndTownSaverService,
+                                                siteInformationService, $translate, navigationService) {
 
-
-        $scope.sitePhoneNumber = siteInformationService.sitePhoneNumber;
-        $scope.whatsapp = siteInformationService.whatsapp;
-        $scope.web = siteInformationService.web;
-        $scope.openingHours = siteInformationService.openingHours;
-        $scope.businessName = siteInformationService.businessName;
-        $scope.businessLocation = siteInformationService.businessLocation;
-        $scope.businessDescription = siteInformationService.businessDescription;
-        $scope.tags = siteInformationService.tags;
-        $scope.businessEmail = siteInformationService.businessEmail;
-        $scope.businessAddress = siteInformationService.businessAddress;
         $scope.businessCategories = siteInformationService.businessCategories;
-        $scope.businessMunicipality = siteInformationService.businessMunicipality;
-
         $scope.showRequiredFieldMessage = false;
-        $scope.waitingRegister = false;
 
+
+        categories.getCategories().then(function (response) {
+            $scope.categories = response;
+        }).catch(function (error) {
+            console.log("Hubo un error", error);
+        });
+
+        $scope.changeViewLocation = function () {
+            if ($scope.registerSiteForm.$valid) {
+                saveDataAndChangeView('/location');
+            } else {
+                $scope.showRequiredFieldMessage = true;
+            }
+        };
+
+        function saveDataAndChangeView(view) {
+            saveSiteInformation();
+            $location.path(view);
+        }
+        function saveSiteInformation() {
+            siteInformationService.businessCategories = $scope.businessCategories;
+        }
 
     });
