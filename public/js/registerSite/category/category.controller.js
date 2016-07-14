02f7fd0c59e8f6c948b0313c48caa1a5ef44c843
AndresDiaz1 <andiazvar@gmail.com>
2016-07-13 19:13:42 -0500

    Se puede desplegar y contraer cada categoria correctamente

--- public/js/registerSite/category/category.controller.js
+++ public/js/registerSite/category/category.controller.js
@@ -21,8 +21,8 @@ angular.module('registerSite')
         $scope.secondAddCategoryButtonIsVisible=true;
 
         $scope.listThirdCategoryIsVisible=false;
-        $scope.compressedSecondCategoryIsVisible=false;
-        $scope.secondAddCategoryButtonIsVisible=true;
+        $scope.compressedThirdCategoryIsVisible=false;
+
 
         $scope.user={
             categorias:''
@@ -46,6 +46,22 @@ angular.module('registerSite')
             }
         }
 
+        $scope.collapseFirstListCategory=function(){
+            $scope.listFirstCategoryIsVisible=false;
+            $scope.compressedFirstCategoryIsVisible=true;
+        }
+
+        $scope.editFirstCategory=function(){
+            $scope.compressedFirstCategoryIsVisible=false;
+            $scope.listFirstCategoryIsVisible=true;
+
+            $scope.listSecondCategoryIsVisible=false;
+            $scope.compressedSecondCategoryIsVisible=true;
+
+            $scope.listThirdCategoryIsVisible=false;
+            $scope.compressedThirdCategoryIsVisible=true;
+        }
+
 
         $scope.getSecondSubCategories=function(){
             if ($scope.businessSecondCategories.category == undefined) {
@@ -60,6 +76,23 @@ angular.module('registerSite')
             }
         }
 
+        $scope.collapseSecondListCategory=function(){
+            $scope.listSecondCategoryIsVisible=false;
+            $scope.compressedSecondCategoryIsVisible=true;
+        }
+
+        $scope.editSecondCategory=function(){
+            $scope.compressedFirstCategoryIsVisible=true;
+            $scope.listFirstCategoryIsVisible=false;
+
+            $scope.listSecondCategoryIsVisible=true;
+            $scope.compressedSecondCategoryIsVisible=false;
+
+            $scope.listThirdCategoryIsVisible=false;
+            $scope.compressedThirdCategoryIsVisible=true;
+        }
+
+
         $scope.getThirdSubCategories=function(){
             if ($scope.businessThirdCategories.category == undefined) {
                 $scope.thirdSubcategories = undefined;
@@ -73,6 +106,22 @@ angular.module('registerSite')
             }
         }
 
+        $scope.collapseThirdListCategory=function(){
+            $scope.listThirdCategoryIsVisible=false;
+            $scope.compressedThirdCategoryIsVisible=true;
+        }
+
+        $scope.editThirdCategory=function(){
+            $scope.compressedFirstCategoryIsVisible=true;
+            $scope.listFirstCategoryIsVisible=false;
+
+            $scope.listSecondCategoryIsVisible=false;
+            $scope.compressedSecondCategoryIsVisible=true;
+
+            $scope.listThirdCategoryIsVisible=true;
+            $scope.compressedThirdCategoryIsVisible=false;
+        }
+
 
 
         $scope.addFirstCategory=function(){
@@ -81,28 +130,25 @@ angular.module('registerSite')
             $scope.firstAddCategoryButtonIsVisible=false;
 
             $scope.listSecondCategoryIsVisible=true;
+            $scope.compressedSecondCategoryIsVisible=false;
 
 
         }
 
         $scope.addSecondCategory=function(){
+            $scope.compressedFirstCategoryIsVisible=true;
+            $scope.listFirstCategoryIsVisible=false;
+
             $scope.compressedSecondCategoryIsVisible=true;
             $scope.listSecondCategoryIsVisible=false;
+
             $scope.secondAddCategoryButtonIsVisible=false;
 
             $scope.listThirdCategoryIsVisible=true;
-
+            $scope.compressedThirdCategoryIsVisible=false;
 
         }
-
-        $scope.editFirstCategory=function(){
-            $scope.compressedFirstCategoryIsVisible=false;
-            $scope.listFirstCategoryIsVisible=true;
-
-            $scope.listSecondCategoryIsVisible=false;
-            $scope.compressedSecondCategoryIsVisible=true;
-        }
-
+        
         $scope.changeViewLocation = function () {
 
             console.log("lo que eligio", $scope.user);
