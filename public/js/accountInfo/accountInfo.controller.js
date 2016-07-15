881b998fd9cc74c6726e4dac8ef76508fe973ef5
Ingeniero 1 <vivelabcrear@gmail.com>
2016-07-15 10:15:49 -0500

    botón atrás del navegador en "mi cuenta"

--- public/js/accountInfo/accountInfo.controller.js
+++ public/js/accountInfo/accountInfo.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('accountInfo')
-    .controller('AccountInfoController', function ($scope, $location,$http,
-                                                   authenticationService, navigationService, siteInformationService, messageService,API_CONFIG) {
+    .controller('AccountInfoController', function ($scope, $location, $http,
+                                                   authenticationService, navigationService, siteInformationService, messageService, API_CONFIG) {
 
         $scope.showRequiredFieldMessage = false;
         $scope.usuario = authenticationService.getUser();
@@ -20,18 +20,18 @@ angular.module('accountInfo')
         }
 
         $scope.addBusiness = function () {
-            navigationService.cameToBusinessInformationThrough='accountinfo';
+            navigationService.cameToBusinessInformationThrough = 'accountinfo';
             $location.path('businessinformation');
         }
-        $scope.editSite = function (sitio)  {
-            
+        $scope.editSite = function (sitio) {
+
             siteInformationService.siteId = sitio.id;
             siteInformationService.sitePhoneNumber = sitio.telefono;
             siteInformationService.whatsapp = sitio.whatsapp;
             siteInformationService.web = sitio.web;
             siteInformationService.openingHours = sitio.horariolocal;
             siteInformationService.businessName = sitio.nombre;
-            siteInformationService.businessLocation = { lat: parseFloat(sitio.latitud), lng: parseFloat(sitio.longitud)};
+            siteInformationService.businessLocation = {lat: parseFloat(sitio.latitud), lng: parseFloat(sitio.longitud)};
             siteInformationService.businessDescription = sitio.descripcion;
             siteInformationService.tags = sitio.tags;
             siteInformationService.businessEmail = sitio.correolocal;
@@ -42,18 +42,18 @@ angular.module('accountInfo')
 
             $location.path('businessinformation');
         }
-        $scope.deleteSite = function (sitio){
-            messageService.confirmMessage("¿Está seguro que desea borrar este sitio?","Borrar sitio", removeSiteFromServer,sitio);
+        $scope.deleteSite = function (sitio) {
+            messageService.confirmMessage("¿Está seguro que desea borrar este sitio?", "Borrar sitio", removeSiteFromServer, sitio);
 
         }
-        function removeSiteFromServer(sitio){
-                $http.delete(API_CONFIG.url + "/sitio/detail/"+sitio.id, siteInformationService.formData,                
+        function removeSiteFromServer(sitio) {
+            $http.delete(API_CONFIG.url + "/sitio/detail/" + sitio.id, siteInformationService.formData,
                 {
                     transformRequest: angular.identity,
                     headers: {'Content-Type': undefined}
                 }).success(function (d) {
-                         $scope.usuario.sitios.splice($scope.usuario.sitios.indexOf(sitio), 1);
-                }).error(function (error) {
+                $scope.usuario.sitios.splice($scope.usuario.sitios.indexOf(sitio), 1);
+            }).error(function (error) {
                 console.log("hubo un error al borrar", error);
 
             });
@@ -62,8 +62,8 @@ angular.module('accountInfo')
         }
 
         $scope.$on('$routeChangeStart', function (scope, next, current) {
-            if(next.$$route.controller=='summaryController') {
-                $location.path('/accountinfo');
+            if (next.$$route.controller == 'summaryController' || next.$$route.controller == 'loginController') {
+                $location.path('/home');
             }
         });
     });
