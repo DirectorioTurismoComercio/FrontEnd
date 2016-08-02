d490640d33268547d201ce18ea1ff07131cdb492
Ingeniero0 <vivelabcrear@gmail.com>
2016-08-02 11:58:54 -0500

    se valida con error e101 que el correo ya existe

--- public/js/registerTrader/registerTrader.controller.js
+++ public/js/registerTrader/registerTrader.controller.js
@@ -69,19 +69,23 @@ angular.module('registerTrader')
                     }).catch(function (errors) {
                         $scope.registerLoading=false;
                         console.log("Errores retornado por el POST de agregar usuario", errors);
-                        if (errors.data.error === 'E101') {
-                            $mdDialog.show(
+                        var error_message='E100';
+                        if (errors.data.email) {
+                           if(errors.data.email[0]==='E101'){
+                                error_message='E101';
+                           }
+                        } 
+           
+                         $mdDialog.show(
                                 $mdDialog.alert()
                                     .parent(angular.element(document.querySelector('#alertPop')))
                                     .clickOutsideToClose(true)
                                     .title('Error')
-                                    .content($translate.instant('E101'))
+                                    .content($translate.instant(error_message))
                                     .ariaLabel('Alert Dialog Demo')
                                     .ok('Aceptar')
                                     .targetEvent('$event')
                             );
-                        } else {
-                        }
                         ;
                     });
                 };
