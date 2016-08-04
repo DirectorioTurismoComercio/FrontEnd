2e2e9770e1e8db5e0127c19399396bb5c70ee7a5
Ingeniero0 <vivelabcrear@gmail.com>
2016-08-04 12:09:32 -0500

    Sale del ignore el archivo constants

---
+++ public/js/constants.js
@@ -0,0 +1,17 @@
+var objeto = {
+        "url": "http://innovamarca.com.co/servicio2",
+        "login": "/rest-auth/login/",
+        "logout": "/rest-auth/logout/",
+        "user": "/rest-auth/registration/",
+        "user_update": "/rest-auth/user/",
+        "sitios":"/ruta/sitios",
+        "sitio":"/sitio",
+        "categorias":"/categorias",
+        "getTownOnMapClickURL":'http://maps.googleapis.com/maps/api/geocode/json?latlng='
+    }
+
+
+
+
+angular.module('constants', [])
+    .constant("API_CONFIG", objeto);
