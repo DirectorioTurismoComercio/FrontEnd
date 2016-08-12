8e568a903bb045ba369b204904d0a6d9c8af069c
Ingeniero 1 <vivelabcrear@gmail.com>
2016-08-12 09:48:49 -0500

    Se vuelve a colocar la restriccion de url

--- public/js/registerSite/registerSite.js
+++ public/js/registerSite/registerSite.js
@@ -6,30 +6,30 @@ angular.module('registerSite', ['ngTagsInput', 'uiGmapgoogle-maps', 'flow', 'ngD
             .when('/businessinformation', {
                 templateUrl: 'js/registerSite/businessInformation/businessinformation.html',
                 controller: 'businessInformationController',
-                //required_roles: 'authenticatedUser'
+                required_roles: 'authenticatedUser'
             })
             .when('/category', {
                 templateUrl: 'js/registerSite/category/category.html',
                 controller: 'categoryController',
-                //required_roles: 'authenticatedUser'
+                required_roles: 'authenticatedUser'
 
             })
             .when('/location', {
                 templateUrl: 'js/registerSite/location/location.html',
                 controller: 'locationController',
-                //required_roles: 'authenticatedUser'
+                required_roles: 'authenticatedUser'
 
             })
             .when('/photos', {
                 templateUrl: 'js/registerSite/photos/photos.html',
                 controller: 'registerPhotosController',
-                //required_roles: 'authenticatedUser'
+                required_roles: 'authenticatedUser'
 
             })
             .when('/summary', {
                 templateUrl: 'js/registerSite/summary/summary.html',
                 controller: 'summaryController',
-                //required_roles: 'authenticatedUser'
+                required_roles: 'authenticatedUser'
 
             })
             ;
