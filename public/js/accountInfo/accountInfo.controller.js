ea161f965116be289ef35b619f716441ec70bb02
Ingeniero0 <vivelabcrear@gmail.com>
2016-08-15 10:52:24 -0500

    se incluye verificación de correo existente en la actualización de los datos de la cuenta

--- public/js/accountInfo/accountInfo.controller.js
+++ public/js/accountInfo/accountInfo.controller.js
@@ -46,7 +46,25 @@ angular.module('accountInfo')
                 )
               .catch(
                     function(errors){
-                        console.log("Errores retornado por el servidor", errors);
+                         $scope.registerLoading=false;
+                        console.log("Errores retornado por el POST de agregar usuario", errors);
+                        var error_message='E100';
+                        if (errors.data.email) {
+                           if(errors.data.email[0]==='E101'){
+                                error_message='E101';
+                           }
+                        } 
+           
+                         $mdDialog.show(
+                                $mdDialog.alert()
+                                    .parent(angular.element(document.querySelector('#alertPop')))
+                                    .clickOutsideToClose(true)
+                                    .title('Error')
+                                    .content($translate.instant(error_message))
+                                    .ariaLabel('Alert Dialog Demo')
+                                    .ok('Aceptar')
+                                    .targetEvent('$event')
+                            );
                     }
                 );
             }
