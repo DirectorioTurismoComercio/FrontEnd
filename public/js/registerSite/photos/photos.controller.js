40afcb62d27a8c29e6dadc3c6535e286fa64756e
Ingeniero 1 <vivelabcrear@gmail.com>
2016-08-17 16:40:26 -0500

    solución bug en editar llegar hasta fotos y retroceder, debe mantener las fotos

--- public/js/registerSite/photos/photos.controller.js
+++ public/js/registerSite/photos/photos.controller.js
@@ -79,6 +79,7 @@ angular.module('registerSite')
 
             if (siteInformationService.URLphotos) {
                 loadPhotosFromServer();
+                savePhotosTemporally();
                 siteInformationService.URLphotos = undefined;
             }
 
