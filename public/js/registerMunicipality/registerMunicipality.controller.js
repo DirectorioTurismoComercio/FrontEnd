b7a0a4c5cef5a8e7c48a46421612a7f6067f22f9
Ingeniero 1 <vivelabcrear@gmail.com>
2016-08-29 17:21:30 -0500

    registro municipio

--- public/js/registerMunicipality/registerMunicipality.controller.js
+++ public/js/registerMunicipality/registerMunicipality.controller.js
@@ -1,19 +1,77 @@
 'use strict';
 
 angular.module('registerMunicipality')
-    .controller('registerMunicipalityController', function ($scope, formValidator) {
+    .controller('registerMunicipalityController', function ($scope, formValidator, $location, ngDialog) {
         $scope.municipalityData = {
             name: undefined,
             email: undefined,
             password: undefined
-        }
+        };
 
-        $scope.$watch('municipalityData.email', function() {
-            try{
-                $scope.isValidEmail=formValidator.isValidEmail($scope.municipalityData.email);
-            }catch (e){}
+        $scope.submitted = false;
+        $scope.registerLoading = false;
+
+
+        $scope.$watch('municipalityData.email', function () {
+            try {
+                $scope.isValidEmail = formValidator.isValidEmail($scope.municipalityData.email);
+            } catch (e) {
+            }
         });
 
+        $scope.goHome = function () {
+            $location.path('home');
+        };
+
+        $scope.save = function () {
+            $scope.submitted = true;
+
+            if ($scope.municipalityData.email != undefined && $scope.isValidEmail && $scope.municipalityData.password != undefined && $scope.municipalityData.password.length >= 6 && $scope.municipalityData.name != undefined) {
+                $scope.registerLoading = true;
+                //redirectToRegisterMunicipality();
+
+                /*var registerPromise;
+                var deferred = $q.defer();
+
+                registerPromise = $http.post(API_CONFIG.url + API_CONFIG.user, {
+                    email: $scope.municipalityData.email,
+                    first_name: $scope.municipalityData.name,
+                    password1: $scope.municipalityData.password,
+                    password2: $scope.municipalityData.password,
+                    last_name: $scope.municipalityData.name
+                });
+
+                registerPromise.then(function (response) {
+                    authenticationService.setUserByToken(response.data.key, deferred).finally(
+                        function() {
+                            redirectToRegisterMunicipality()
+                        }
+                    );
+                });
+
+                registerPromise.catch(function(e){
+                    $scope.registerLoading = false;
+                    formValidator.emailAlreadyExistsShowError(e);
+                });*/
+            };
+        }
+
+        $scope.doneRegistration = function () {
+            ngDialog.close();
+            //$location.path('/businessinformation');
+        }
+
+        function redirectToRegisterMunicipality(){
+            $scope.registerLoading=false;
+            ngDialog.open({
+                template: 'js/registerMunicipality/completeMunicipalityRegistration.html',
+                width: 'auto',
+                showClose: false,
+                scope: $scope,
+                closeByEscape: false,
+                closeByDocument: false
+            });
+        }
 
 
     });
--- public/js/registerMunicipality/registerMuniciplaity.controller.spec.js
+++ public/js/registerMunicipality/registerMuniciplaity.controller.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Controller: registerMunicipalityController', function () {
-    var registerMunicipalityController, $scope, deferred, testformValidator;
+    var registerMunicipalityController, $scope, deferred, testformValidator, testLocation, testngDialog;
 
     beforeEach(module('gemStore'));
     beforeEach(module('registerMunicipality'));
@@ -22,14 +22,21 @@ describe('Controller: registerMunicipalityController', function () {
     }));
 
 
-    beforeEach(inject(function ($controller,$q,$rootScope, formValidator) {
+    beforeEach(inject(function ($controller,$q,$rootScope, formValidator, $location, ngDialog) {
         $scope = $rootScope.$new();
         deferred = $q.defer();
         testformValidator=formValidator;
+        testLocation=$location;
+        testngDialog=ngDialog;
+
+        spyOn($location,'path');
+        spyOn(formValidator,'emailAlreadyExistsShowError');
+        spyOn(ngDialog,'open');
 
         registerMunicipalityController = $controller('registerMunicipalityController', {
             $scope: $scope,
-            formValidator:testformValidator
+            formValidator:testformValidator,
+            ngDialog:testngDialog
 
         });
     }));
@@ -44,5 +51,48 @@ describe('Controller: registerMunicipalityController', function () {
         $scope.$digest();
         expect($scope.isValidEmail).toBe(false);
     });
+
+    it('Should redirect to home when user click goHome button', function () {
+        $scope.goHome();
+        expect(testLocation.path).toHaveBeenCalled();
+    });
+
+    it('Should set to True submitted when user clicks RegisterButton', function () {
+        $scope.save();
+        expect($scope.submitted).toBe(true);
+    });
+
+    it('Should not show registerLoading when user clicks save and form fields are not valids', function () {
+        $scope.municipalityData = {
+            name: undefined,
+            email: undefined,
+            password: undefined
+        };
+        $scope.municipalityData.email='town@';
+        $scope.$digest();
+
+        $scope.save();
+        expect($scope.registerLoading).toBe(false);
+    });
+
+    it('Should set to True submitted when user clicks RegisterButton', function () {
+        $scope.save();
+        expect($scope.submitted).toBe(true);
+    });
+
+    it('Should show registerLoading when user clicks save and form fields are valids', function () {
+        setValidFormFields($scope);
+        $scope.save();
+        expect($scope.registerLoading).toBe(true);
+    });
+
+    function setValidFormFields($scope) {
+        $scope.municipalityData = {
+            name: 'chía',
+            email: 'chia@chia.com',
+            password: '1234567'
+        };
+        $scope.$digest();
+    }
 
 });
