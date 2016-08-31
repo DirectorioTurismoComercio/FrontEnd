ef30cd83b9006603195418c34546bf2fd5fc0575
Ingeniero 1 <vivelabcrear@gmail.com>
2016-08-31 16:20:01 -0500

    Se colocan las funciones del controlador de fotos de municipio

--- public/js/municipality/registerMunicipalityInformation/municipalityPhotos/municipalityPhotos.controller.js
+++ public/js/municipality/registerMunicipalityInformation/municipalityPhotos/municipalityPhotos.controller.js
@@ -61,9 +61,84 @@ angular.module('Municipality')
                 savePhotosTemporally();
                 municipalityInformationService.setMunicipalityURLPhotos(undefined);
             }
-
-
         }
+
+        function loadPhotosFromServer() {
+            var i;
+            $scope.loadingPhotos = true;
+            numPhotos = municipalityInformationService.getMunicipalityURLPhotos().length;
+            for (i = 0; i < numPhotos; i++) {
+                loadPhotoFromURL(municipalityInformationService.getMunicipalityURLPhotos()[i].URLfoto, municipalityInformationService.getMunicipalityURLPhotos()[i].tipo);
+            }
+        }
+
+        function loadPhotoFromURL(urlPhoto, tipo) {
+
+            var arg = "?randnum=1"
+
+            $http({
+                method: 'GET',
+                url: urlPhoto + arg,
+                responseType: "arraybuffer"
+            }).success(function (data) {
+                var arrayBufferView = new Uint8Array(data);
+                var blob = new Blob([arrayBufferView], {type: "image/jpeg"});
+                blob.name = 'RegistroIconMenu.jpg';
+                blob.lastModifiedDate = new Date();
+                var file;
+                var flowPhotos = getFlow(tipo);
+                file = new Flow.FlowFile(flowPhotos.flow, blob);
+                flowPhotos.flow.files.push(file);
+                loadedPhotos++;
+                if (loadedPhotos == numPhotos) {
+                    $scope.loadingPhotos = false;
+                }
+            }).error(function (error) {
+                console.log("hubo un error al cargar la foto", error);
+            });
+        }
+
+        function getFlow(photoType) {
+
+            if (photoType == 'P') return $scope.flowMunicipalityMainPhoto;
+            if (photoType == 'F') return $scope.flowMunicipalityFacedePhotos;
+            if (photoType == 'I') return $scope.flowCoatArmsPhoto;
+
+            return $scope.flowMainPhoto;
+        }
+
+        function savePhotosTemporally() {
+            municipalityInformationService.setMunicipalityMainPhoto($scope.flowMunicipalityMainPhoto.flow.files);
+            municipalityInformationService.setMunicipalityCoatArmsPhoto($scope.flowCoatArmsPhoto.flow.files);
+            municipalityInformationService.setMunicipalityFacadePhotos($scope.flowMunicipalityFacedePhotos.flow.files);
+        }
+
+        $scope.correctOrientation = function (orientation) {
+            var style = '';
+
+            switch (orientation) {
+                case 3:
+                    style = 'rotate180';
+                    break;
+                case 4:
+                    style = 'rotate180';
+                    break;
+                case 5:
+                    style = 'rotate90';
+                    break;
+                case 6:
+                    style = 'rotate90';
+                    break;
+                case 7:
+                    style = 'rotate270';
+                    break;
+                case 8:
+                    style = 'rotate270';
+                    break;
+            }
+
+            return style;
+        };
 
 
     });
