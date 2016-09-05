641692de2155680617186be1da32e9aac3bbfb02
Ingeniero 1 <vivelabcrear@gmail.com>
2016-09-05 14:37:07 -0500

    Se corrige variable de timeout

--- public/js/constants.js
+++ public/js/constants.js
@@ -9,7 +9,7 @@ var objeto = {
         "new_password":"/rest-auth/password/change/",
         "categorias":"/categorias",
         "getTownOnMapClickURL":'http://maps.googleapis.com/maps/api/geocode/json?latlng=',
-        "timeout":3
+        "timeout":30000
     }
 
 
