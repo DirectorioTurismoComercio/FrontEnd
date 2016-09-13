0bbf8884629f99b3050eab7a5f6fdbb6855c565c
Ingeniero 1 <vivelabcrear@gmail.com>
2016-09-13 10:42:20 -0500

    Se verifica el login del usuario antes de hacer otro inicio de sesión

--- public/js/footer/footer.controller.js
+++ public/js/footer/footer.controller.js
@@ -1,25 +1,44 @@
 angular.module('appFooter')
     .controller('footerController', function ($scope, authenticationService, messageService, $location) {
 
-        var alreadyLoggedIn = authenticationService.getUser();
+        var loggedUser = authenticationService.getUser();
 
-        $scope.goToRegisterTrader=function(){
-            if(alreadyLoggedIn){
+        $scope.goToRegisterTrader = function (path) {
+            checkLoggedUserForRegistration(path);
+        };
+
+
+        $scope.goToRegisterMunicipality = function (path) {
+            checkLoggedUserForRegistration(path);
+        };
+
+        $scope.goToLoginTrader = function () {
+            checkLoggedUserForLogIn("C",'/login','/accountinfo');
+        };
+
+        $scope.goToLoginMunicipality = function (){
+            checkLoggedUserForLogIn("M",'/loginmunicipality','/municipalityaccountinfo');
+        };
+
+        function checkLoggedUserForRegistration(path){
+            if (loggedUser) {
                 messageService.showErrorMessage("DOUBLE_REGISTER_ERROR");
-            }else{
-                $location.path('/registertrader');
+            } else {
+                $location.path(path);
             }
         }
 
-
-        $scope.goToRegisterMunicipality=function(){
-            if(alreadyLoggedIn){
-                messageService.showErrorMessage("DOUBLE_REGISTER_ERROR");
-            }else{
-                $location.path('/registermunicipality');
+        function checkLoggedUserForLogIn(accountType, pathLogin, pathAccountInfo){
+            if (loggedUser) {
+                if(loggedUser.tipo_cuenta==accountType){
+                    $location.path(pathAccountInfo);
+                }else{
+                    messageService.showErrorMessage("DOUBLE_LOGIN_ERROR");
+                }
+            } else {
+                $location.path(pathLogin);
             }
         }
-
 
     });
 
