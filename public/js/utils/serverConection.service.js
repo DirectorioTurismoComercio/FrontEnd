b3db333560da337fc1d13a2a90f0a8d0d834c4cc
Ingeniero 1 <vivelabcrear@gmail.com>
2016-09-20 11:26:25 -0500

    refactor borrar rutas

--- public/js/utils/serverConection.service.js
+++ public/js/utils/serverConection.service.js
@@ -3,13 +3,20 @@
 angular.module('utils')
     .service('serverConnectionService', function (messageService) {
 
+        return {
+            checkTimeOutError: checkTimeOutError,
+            communicationError:communicationError
+        }
+
         function checkTimeOutError(e){
             if(e.status===0 && e.statusText==""){
                 messageService.showErrorMessage("TIMEOUT_CONNECTION_ERROR",true);
             }
         }
 
-        return {
-            checkTimeOutError: checkTimeOutError
+        function communicationError(error){
+            messageService.showErrorMessage("ERROR_SERVER_COMMUNICATION", true);
+            console.log("hubo un error al borrar", error);
         }
+
     });
