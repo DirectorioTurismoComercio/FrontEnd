be88ff55499a640054e884dbc2193ef735d2c88b
Ingeniero0 <vivelabcrear@gmail.com>
2016-10-12 12:01:55 -0500

    refactor reducción de fotos completado

--- public/js/registerSite/photos/photos.controller.js
+++ public/js/registerSite/photos/photos.controller.js
@@ -9,6 +9,7 @@ angular.module('registerSite')
 
         var max_width=1199;
         var max_height=899;
+        var max_size=500000;
 
         $scope.$on('$viewContentLoaded', function () {
             checkSelectedPhotos();
@@ -72,76 +73,72 @@ angular.module('registerSite')
                 exif_orientation = orientation;
 
                 if(flowFile.file.size>500000){
-                processImage(flowFile);
+                    if(flowFile.file.new_dimensions){
+                       if (flowFile.file.new_dimensions.width>max_width || flowFile.file.new_dimensions.height>max_height) 
+                       {
+                        reduceImage(flowFile);
+                       }
+                    }else{
+                        reduceImage(flowFile);
+                    }
+
                 }
                 
-                $scope.$apply();
+                
             });
         };
         
         
-        function processImage(flowFile){
-        
-        var width;
-        var height;
-        var new_width;
-        var new_height;
-
-        
-
-        var fileReader = new FileReader();
-        var image = new Image();
-        var scale_factor=1;
-
-        fileReader.onload = function (event) {
-            var uri = event.target.result;
-            image.src = uri;
-            image.onload = function(){
-            if(exif_orientation==6 || exif_orientation==8){  
-                height = this.width;
-                width = this.height;
-                
-             }else{
-                width = this.width;
-                height = this.height;
-               
-             }   
-
-            if (width>max_width || height>max_height){
-                if (width>height){
-                    scale_factor = width/max_width;
-                    console.log("width - max-width"+width+" "+max-width);
-                }
-                else {
-                    scale_factor = height/max_height;
-                    console.log("height - max-h"+height+" "+max_height);
-                }
-                console.log("factor",scale_factor);
-            if(exif_orientation==6 || exif_orientation==8){  
-                 new_height = parseInt(width/scale_factor)
-
-                new_width = parseInt(height/scale_factor)
-                
-             }else{
-                new_width = parseInt(width/scale_factor)
-
-                new_height = parseInt(height/scale_factor)
-             }   
-
-               
-            }
-            resizeFlowFile(flowFile, new_width, new_height);            
-
-                
-            };
-            
-        };
-        fileReader.readAsDataURL(flowFile.file);
-        
+function reduceImage(flowFile){
+    var new_width;
+    var new_height;
+    var fileReader = new FileReader();
+    var image = new Image();
+    fileReader.onload = function (event) {
+        var uri = event.target.result;
+        image.src = uri;
+        image.onload = function(){
+            var scale_factor = calculateScaleFactor(this.width,this.height,max_width,max_height, flowFile.orientation);    
+            new_width = parseInt(this.width/scale_factor);
+            new_height = parseInt(this.height/scale_factor);  
+            resizeFlowFile(flowFile, new_width, new_height);    
         }
+                  
+    };
+    fileReader.readAsDataURL(flowFile.file);
+}
+
+function calculateScaleFactor(current_width, current_height, max_width, max_height, orientation){
+    var height;
+    var width;
+    var scale_factor=1;
+    if(orientation==6 || orientation==8){  
+        height = current_width;
+        width = current_height;
+        
+     }else{
+        width = current_width;
+        height = current_height;
+       
+     }   
+
+    if (width>max_width || height>max_height){
+        if (width>height){
+            scale_factor = width/max_width;
+        }
+        else {
+            scale_factor = height/max_height;
+        }
+    }
+    return scale_factor;
+}
 
 function resizeFlowFile(flowFile, new_width, new_height){
  var src = URL.createObjectURL(flowFile.file);   
+ console.log("new height",new_height);
+ flowFile.width = new_width;
+ flowFile.height = new_height;
+ console.log("resize",flowFile);
  resizeService.resizeImage(src, {
                 width: new_width,
                 height: new_height,
@@ -165,9 +162,11 @@ function createAndReplaceResizedFlowFile(image,flowFile){
     var blob = dataURItoBlob(image);
     blob.name = 'blob';
     blob.lastModifiedDate = new Date();
-    var resizedFlowFile = new Flow.FlowFile(flowFile.flowObj, new File([blob], flowFile.file.name.toLowerCase()+"_exif_orientation:"+exif_orientation, {type: "image/jpeg", lastModified: Date.now()}));
+    var resizedFlowFile = new Flow.FlowFile(flowFile.flowObj, new File([blob], flowFile.file.name.toLowerCase()+"_exif_orientation:"+flowFile.orientation, 
+                                {type: "image/jpeg", lastModified: Date.now()}));
     replaceFlowFile(resizedFlowFile, flowFile)        
 }
+
 function replaceFlowFile(resizedFlowFile, flowFile){
     for(var i=0;i<flowFile.flowObj.files.length;i++){
                  
@@ -175,7 +174,10 @@ function replaceFlowFile(resizedFlowFile, flowFile){
         flowFile.flowObj.files[i]= resizedFlowFile;
         flowFile.flowObj.files[i].file.exifdata = flowFile.file.exifdata;
         flowFile.flowObj.files[i].file.iptcdata = flowFile.file.iptcdata;
-        
+        flowFile.flowObj.files[i].file.new_dimensions= new Object();
+        flowFile.flowObj.files[i].file.new_dimensions.width = flowFile.width;
+        flowFile.flowObj.files[i].file.new_dimensions.height = flowFile.height;
+        console.log("flowFile.flowObj.files[i].file",flowFile.flowObj.files[i].file);
         }
     }
 }                                    
