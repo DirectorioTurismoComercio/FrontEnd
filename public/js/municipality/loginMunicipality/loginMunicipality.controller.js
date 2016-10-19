6b37ad9277fca9d5ca6fe14e42326c4a60e3806c
Estudiante Auxiliar <vivelabcrear@gmail.com>
2016-10-19 15:54:14 -0500

    Se agrega pantalla de cuenta desactivada en el municipio

--- public/js/municipality/loginMunicipality/loginMunicipality.controller.js
+++ public/js/municipality/loginMunicipality/loginMunicipality.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('Municipality')
-    .controller('loginMunicipalityController', function ($scope, $auth,authenticationService, $mdDialog,messageService, $translate, $location, navigationService) {
+    .controller('loginMunicipalityController', function ($scope, $auth, authenticationService,
+                                                         $mdDialog, messageService, $translate,
+                                                         $location, navigationService, PopupService) {
         $scope.submitted = false;
 
         $scope.loginMunicipalityFields = {
@@ -12,15 +14,15 @@ angular.module('Municipality')
         var alreadyLoggedIn = authenticationService.getUser();
 
         $scope.forgottenPassword = function () {
-            navigationService.cameToRecoveryPasswordThrough="MunicipalityUserLogin";
+            navigationService.cameToRecoveryPasswordThrough = "MunicipalityUserLogin";
             $location.path('/forgotpassword');
         }
 
         $scope.loginMunicipality = function () {
             $scope.submitted = true;
-            if(alreadyLoggedIn){
-                messageService.showErrorMessage("DOUBLE_LOGIN_ERROR",true);
-            }else{
+            if (alreadyLoggedIn) {
+                messageService.showErrorMessage("DOUBLE_LOGIN_ERROR", true);
+            } else {
                 if ($scope.loginMunicipalityFields.email != undefined && $scope.loginMunicipalityFields.password != undefined && $scope.loginMunicipalityFields.password.length >= 6) {
                     authenticationService.login({
                             email: $scope.loginMunicipalityFields.email,
@@ -28,23 +30,45 @@ angular.module('Municipality')
                         })
                         .then(function () {
                             $scope.user = authenticationService.getUser();
-                            if($scope.user.tipo_cuenta!="M"){
-                                showErrorDialog($translate.instant("INCORRECT_ACCOUNT_TYPE_TRADER"));
-                                $auth.logout();
-                                $auth.removeToken();
-                                authenticationService.logout();
-                            }else{
-                                redirectToProfileMain();
-                            }
-
+                            checkUserLogged();
                         }).catch(function (error) {
-                        messageService.showErrorMessage("BAD_LOGIN",true);
+                        messageService.showErrorMessage("BAD_LOGIN", true);
                     });
                 }
                 else {
-                    messageService.showErrorMessage("LOGIN_FIELDS_INCOMPLETE",true);
+                    messageService.showErrorMessage("LOGIN_FIELDS_INCOMPLETE", true);
                 }
             }
+        };
+
+        function checkUserLogged() {
+            if ($scope.user.tipo_cuenta != "M") {
+                checkMunicipalityAccount();
+            } else if (!$scope.user.es_cuenta_activa) {
+                checkInactiveAccount();
+            }
+            else {
+                redirectToProfileMain();
+            }
+        }
+
+        function checkMunicipalityAccount() {
+            showErrorDialog($translate.instant("INCORRECT_ACCOUNT_TYPE_TRADER"));
+            invalidateLogin();
+        }
+
+        function checkInactiveAccount() {
+            PopupService.showYesMessage("REACTIVATE_MUNICIPALITY_ACCOUNT.TITLE",
+                "REACTIVATE_MUNICIPALITY_ACCOUNT.MESSAGE",
+                "REACTIVATE_MUNICIPALITY_ACCOUNT.CONTINUE").then(function () {
+                invalidateLogin();
+            });
+        }
+
+        function invalidateLogin() {
+            $auth.logout();
+            $auth.removeToken();
+            authenticationService.logout();
         }
 
         function redirectToProfileMain() {
--- public/js/popup/popup.service.js
+++ public/js/popup/popup.service.js
@@ -37,8 +37,36 @@ angular.module('popup', [])
             return deferred.promise;
         }
 
+        function showYesMessage(titleId, messageId, yesLabelId) {
+            var deferred = $q.defer();
+
+            var options = {
+                title: titleId,
+                message: messageId,
+                yesLabel: yesLabelId,
+                yesCallback: function () {
+                    ngDialog.close();
+                    deferred.resolve();
+                }
+            };
+
+            angular.extend(scope, options);
+
+            ngDialog.open({
+                template: 'js/popup/template.html',
+                width: 'auto',
+                showClose: false,
+                scope: scope,
+                closeByEscape: true,
+                closeByDocument: true
+            });
+
+            return deferred.promise;
+        }
+
 
         return {
             showYesNoMessage: showYesNoMessage,
+            showYesMessage: showYesMessage
         }
     });
