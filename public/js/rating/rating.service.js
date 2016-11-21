354bc5f14332df66dbc9fcf36e418e9a27d215f7
Víctor Rodriguez <vrodmz@gmail.com>
2016-11-21 09:05:54 -0500

    Se quita boton para calificar

--- public/js/rating/rating.service.js
+++ public/js/rating/rating.service.js
@@ -1,28 +1,37 @@
 'use strict';
 
-angular.module('gemStore').service('RatingService', 
-		function (authenticationService,$http,API_CONFIG) {
-    function rateSite(siteId, rating) {
-        
-        $http.post(API_CONFIG.url + API_CONFIG.rate, {
-                        sitio: siteId,
-                        calificacion: rating
-                    },
-                    {
-                       headers: {
-                            'Authorization': 'Token ' + authenticationService.getUser().token
-                        }
-                    }).then(function (response) {
-                        
-                    
+angular.module('gemStore').service('RatingService',
+    function (authenticationService, $http, API_CONFIG) {
+        function sendRate(siteId, rating, user) {
+            $http.post(API_CONFIG.url + API_CONFIG.rate, {
+                    sitio: siteId,
+                    calificacion: rating
                 },
-                function (error){
+                {
+                    headers: {
+                        'Authorization': 'Token ' + user.token
+                    }
+                }).then(function (response) {
+
+
+                },
+                function (error) {
                     console.log(error);
                 }
-                );
-    }
+            );
+        }
 
-    return {
-        rateSite: rateSite
-    };
-});
+        function rateSite(siteId, rating) {
+            var user = authenticationService.getUser();
+
+            if (user) {
+                sendRate(siteId, rating, user);
+            } else {
+                alert("Primero debe iniciar sesión para calificar un sitio");
+            }
+        }
+
+        return {
+            rateSite: rateSite
+        };
+    });
